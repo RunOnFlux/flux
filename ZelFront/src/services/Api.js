f0a435feb7de31a0d848d4f8268175331e849b2e
TheTrunk <tadeas.kmenta@zel.cash>
2019-08-02 14:10:45 +0200

    fixes

--- ZelFront/src/services/Api.js
+++ ZelFront/src/services/Api.js
@@ -1,9 +1,11 @@
 
 import axios from 'axios'
+const ip = require('ip')
 const config = require('../../../config/default')
 const port = config.server.localport
+const ipaddr = ip.address()
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:${port}`
+    baseURL: `http://${ipaddr}:${port}`
   })
 }
