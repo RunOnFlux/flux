125c46e90b5659d5d2faf5d6c5ab422e68f19236
TheTrunk <tadeas.kmenta@zel.cash>
2019-08-29 15:55:02 +0200

    navigation preparation

--- ZelFront/src/components/shared/Header.vue
+++ ZelFront/src/components/shared/Header.vue
@@ -1,12 +1,46 @@
 <template>
-  <div class="header-grid">
-    <div class="header-logo">
-      <span class="helper"></span>
-      <img src="@/assets/img/ZelNodes.svg">
-    </div>
-    <div class="header-menu">
-      {{ privilage }}
-    </div>
+  <div class="header-menu">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#333333"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
+      <el-menu-item index="0">
+        <div class='header-logo'>
+          <span class="helper"></span>
+          <img src="@/assets/img/ZelNodes.svg">
+        </div>
+      </el-menu-item>
+      <el-submenu index="1">
+        <template slot="title">ZelCash</template>
+        <el-menu-item index="1-1">Get Info</el-menu-item>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">ZelNode</template>
+        <el-menu-item index="2-1">ZelNode Status</el-menu-item>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">ZelFlux</template>
+        <el-menu-item index="3-1">Flux Info</el-menu-item>
+      </el-submenu>
+      <el-submenu index="10">
+        <template slot="title">Admin</template>
+        <el-menu-item index="10-1">Update Flux</el-menu-item>
+        <el-menu-item index="10-2">Rebuild Flux</el-menu-item>
+        <el-menu-item index="10-3">Logged Users</el-menu-item>
+        <el-menu-item index="10-3">Active Login Phrases</el-menu-item>
+        <el-submenu index="10-4">
+          <template slot="title">Logout options</template>
+          <el-menu-item index="10-4-1">Logout current session</el-menu-item>
+          <el-menu-item index="10-4-2">Logout all sessions</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="4">Say Hi!</el-menu-item>
+    </el-menu>
   </div>
 </template>
 
@@ -16,6 +50,16 @@ export default {
   name: 'Header',
   props: {
     privilage: String,
+  },
+  data() {
+    return {
+      activeIndex: '0',
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
   },
 };
 </script>
