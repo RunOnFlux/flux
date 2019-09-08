0809155a344104fd3e0592f51c1ced1450f42ce8
TheTrunk <tadeas.kmenta@zel.cash>
2019-09-08 18:46:09 +0200

    provide zelcash update script with apt info

--- helpers/updateZelCash.sh
+++ helpers/updateZelCash.sh
@@ -11,6 +11,13 @@ sudo $COIN_CLI stop > /dev/null 2>&1 && sleep 5
 sudo systemctl stop zelcash > /dev/null 2>&1 && sleep 3
 sudo killall $COIN_DAEMON > /dev/null 2>&1
 
+#Make sure our key and apt is present
+echo 'deb https://apt.zel.cash/ all main' | sudo tee /etc/apt/sources.list.d/zelcash.list
+gpg --keyserver keyserver.ubuntu.com --recv 4B69CA27A986265D > /dev/null 2>&1
+gpg --keyserver na.pool.sks-keyservers.net --recv 4B69CA27A986265D > /dev/null 2>&1
+gpg --keyserver eu.pool.sks-keyservers.net --recv 4B69CA27A986265D > /dev/null 2>&1
+gpg --export 4B69CA27A986265D | sudo apt-key add -
+
 #Updating zelcash package
 sudo apt-get update -y
 sudo apt-get install zelcash -y
