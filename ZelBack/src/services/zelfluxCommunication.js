74064f5551f05154974916bbe21cd744fe268738
TheTrunk <tadeas.kmenta@zel.cash>
2019-11-03 06:12:58 +0100

    test autoupdate

--- ZelBack/src/services/zelfluxCommunication.js
+++ ZelBack/src/services/zelfluxCommunication.js
@@ -4,6 +4,7 @@ const log = require('../lib/log');
 const serviceHelper = require('./serviceHelper');
 const zelcashServices = require('./zelcashService');
 const config = require('../../../config/default');
+console.log(require('bitgo-utxo-lib'));
 
 const outgoingConnections = [];
 
