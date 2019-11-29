58edd99724a8bdebc759db7ae533531127b52c68
TheTrunk <tadeas.kmenta@zel.cash>
2019-11-29 11:00:30 +0100

    comment out https

--- ZelBack/server.js
+++ ZelBack/server.js
@@ -1,20 +1,20 @@
 // ZelBack configuration
 const config = require('config');
-const fs = require('fs');
-const https = require('https');
-const path = require('path');
+// const fs = require('fs');
+// const https = require('https');
+// const path = require('path');
 const app = require('./src/lib/server.js');
 const log = require('./src/lib/log');
 const communication = require('./src/services/zelfluxCommunication');
 
-const key = fs.readFileSync(path.join(__dirname, '../certs/selfsigned.key'), 'utf8');
-const cert = fs.readFileSync(path.join(__dirname, '../certs/selfsigned.crt'), 'utf8');
-const credentials = { key, cert };
-const httpsServer = https.createServer(credentials, app);
+// const key = fs.readFileSync(path.join(__dirname, '../certs/selfsigned.key'), 'utf8');
+// const cert = fs.readFileSync(path.join(__dirname, '../certs/selfsigned.crt'), 'utf8');
+// const credentials = { key, cert };
+// const httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(config.server.apiporthttps, () => {
-  log.info(`ZelBack https listening on port ${config.server.apiporthttps}!`);
-});
+// httpsServer.listen(config.server.apiporthttps, () => {
+//   log.info(`ZelBack https listening on port ${config.server.apiporthttps}!`);
+// });
 
 app.listen(config.server.apiport, () => {
   log.info(`ZelBack listening on port ${config.server.apiport}!`);
--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 // ZelBack configuration
 const config = require('config');
-const fs = require('fs');
-const https = require('https');
+// const fs = require('fs');
+// const https = require('https');
 const path = require('path');
 const express = require('express');
 const app = require('./ZelBack/src/lib/server.js');
 const log = require('./ZelBack/src/lib/log');
 const communication = require('./ZelBack/src/services/zelfluxCommunication');
 
-const key = fs.readFileSync(path.join(__dirname, './certs/selfsigned.key'), 'utf8');
-const cert = fs.readFileSync(path.join(__dirname, './certs/selfsigned.crt'), 'utf8');
-const credentials = { key, cert };
-const httpsServer = https.createServer(credentials, app);
+// const key = fs.readFileSync(path.join(__dirname, './certs/selfsigned.key'), 'utf8');
+// const cert = fs.readFileSync(path.join(__dirname, './certs/selfsigned.crt'), 'utf8');
+// const credentials = { key, cert };
+// const httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(config.server.apiporthttps, () => {
-  log.info(`ZelBack https listening on port ${config.server.apiporthttps}!`);
-});
+// httpsServer.listen(config.server.apiporthttps, () => {
+//   log.info(`ZelBack https listening on port ${config.server.apiporthttps}!`);
+// });
 
 app.listen(config.server.apiport, () => {
   log.info(`ZelBack running on port ${config.server.apiport}!`);
