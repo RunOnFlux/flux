9ec8a8ab36a7005399d821bc013dbcdc4b517acf
TheTrunk <tadeas.kmenta@zel.cash>
2019-12-08 12:48:51 +0100

    add comments about gzip/brotli

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
 const path = require('path');
+// ideally https://github.com/webpack-contrib/compression-webpack-plugin#using-brotli from nodejs 11.7.0
+// const BrotliPlugin = require('brotli-webpack-plugin');
+// const CompressionPlugin = require('compression-webpack-plugin');
+// const zopfli = require('@gfx/zopfli');
+
+
+const plugins = [];
+// if (process.env.NODE_ENV === 'production') {
+//   const compressionTest = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
+//   plugins = [
+//     new CompressionPlugin({
+//       algorithm(input, compressionOptions, callback) {
+//         return zopfli.gzip(input, compressionOptions, callback);
+//       },
+//       compressionOptions: {
+//         numiterations: 15,
+//       },
+//       minRatio: 0.99,
+//       test: compressionTest,
+//     }),
+//     new BrotliPlugin({
+//       test: compressionTest,
+//       minRatio: 0.99,
+//     }),
+//   ];
+// }
 
 module.exports = {
   chainWebpack: (config) => {
@@ -20,5 +46,8 @@ module.exports = {
       // output as dist/index.html
       filename: 'index.html',
     },
+  },
+  configureWebpack: {
+    plugins,
   },
 };
