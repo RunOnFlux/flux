fbc7dba46062d6f61b30af7394149f17ca529731
TheTrunk <tadeas.kmenta@zel.cash>
2019-12-15 21:33:48 +0100

    more tests

--- tests/ZelBack/apiTests.js
+++ tests/ZelBack/apiTests.js
@@ -1,7 +1,11 @@
 const request = require('supertest');
 const config = require('config');
+const chai = require('chai');
+const expect = chai.expect;
 const app = require('../../ZelBack/src/lib/server.js');
 const log = require('../../ZelBack/src/lib/log');
+const packageJson = require('../../package.json');
+const { version } = packageJson;
 
 const server = app.listen(config.server.apiport, () => {
   log.info(`ZelBack listening on port ${config.server.apiport}!`);
@@ -10,15 +14,35 @@ const server = app.listen(config.server.apiport, () => {
 describe('loading express', function () {
   after(function (done) {
     server.close(done);
-  }); 
+  });
   it('/zelflux/version', function testSlash(done) {
     request(server)
       .get('/zelflux/version')
-      .expect(200, done);
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        expect(JSON.parse(res.text).data).to.be.equal(version);
+        return done();
+      });
   });
-  it('/zelid/loginphrase', function testPath(done) {
+  it('non-existing-path', function testPath(done) {
     request(server)
       .get('/foo/bar')
       .expect(404, done);
+  });
+  it('/zelid/loginphrase', function testSlash(done) {
+    request(server)
+      .get('/zelid/loginphrase')
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        expect(JSON.parse(res.text).status).to.be.equal('success');
+        expect(JSON.parse(res.text).data.charAt(0)).to.be.equal('1');
+        return done();
+      });
   });
 });
