6f81fc19d0e48038ce85bc01417571f0f98c7634
TheTrunk <tadeas.kmenta@zel.cash>
2020-03-16 12:37:22 +0700

    fix eslint

--- ZelFront/src/components/ZelNode.vue
+++ ZelFront/src/components/ZelNode.vue
@@ -290,9 +290,9 @@ export default {
       this.getZelNodeStatusResponse.data = response.data.data;
       console.log(this.getZelNodeStatusResponse.data);
       if (this.getZelNodeStatusResponse.data) {
-        if (this.getZelNodeStatusResponse.data.location === "CONFIRMED") {
+        if (this.getZelNodeStatusResponse.data.location === 'CONFIRMED') {
           this.getZelNodeStatusResponse.zelnodeStatus = 'ZelNode is working correctly';
-        } else if (this.getZelNodeStatusResponse.data.location === "STARTED") {
+        } else if (this.getZelNodeStatusResponse.data.location === 'STARTED') {
           this.getZelNodeStatusResponse.zelnodeStatus = 'ZelNode has just been started. ZelFlux is running with limited capabilities.';
         } else {
           this.getZelNodeStatusResponse.zelnodeStatus = 'ZelNode is not confirmed. ZelFlux is running with limited capabilities.';
@@ -300,8 +300,8 @@ export default {
       }
       if (this.getZelNodeStatusResponse.data.collateral) {
         const collateral = this.getZelNodeStatusResponse.data.collateral.split(', ');
-        const collateralHash = collateral[0].split("COutPoint(")[1];
-        const collateralIndex = collateral[1].split(")")[0];
+        const collateralHash = collateral[0].split('COutPoint(')[1];
+        const collateralIndex = collateral[1].split(')')[0];
         const rawTxResponse = await ZelCashService.getRawTransaction(collateralHash);
         const rawtxdata = rawTxResponse.data.data;
         const address = rawtxdata.vout[collateralIndex].scriptPubKey.addresses[0];
