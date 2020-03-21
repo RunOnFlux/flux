526001b818a39da75867e00688dddccae671f59d
Restyled.io <commits@restyled.io>
2020-03-21 10:57:24 +0000

    Restyled by shfmt

--- helpers/restartZelCash.sh
+++ helpers/restartZelCash.sh
@@ -5,8 +5,8 @@ COIN_DAEMON='zelcashd'
 COIN_CLI='zelcash-cli'
 
 #Closing zelcash daemon
-sudo "$COIN_CLI" stop > /dev/null 2>&1 && sleep 5
-sudo systemctl stop zelcash > /dev/null 2>&1 && sleep 3
-sudo killall "$COIN_DAEMON" > /dev/null 2>&1
+sudo "$COIN_CLI" stop >/dev/null 2>&1 && sleep 5
+sudo systemctl stop zelcash >/dev/null 2>&1 && sleep 3
+sudo killall "$COIN_DAEMON" >/dev/null 2>&1
 sleep 2
 "$COIN_DAEMON"
