ca2f94da2ec4a174b32cca1fe63366bc52c66396
dk808 <41762330+dk808@users.noreply.github.com>
2020-03-21 03:52:31 -0700

    Update restartZelCash.sh

--- helpers/restartZelCash.sh
+++ helpers/restartZelCash.sh
@@ -2,15 +2,11 @@
 
 #information
 COIN_DAEMON='zelcashd'
-BENCH_DAEMON='zelbenchd'
 COIN_CLI='zelcash-cli'
-#end of required details
 
 #Closing zelcash daemon
 sudo $COIN_CLI stop > /dev/null 2>&1 && sleep 5
 sudo systemctl stop zelcash > /dev/null 2>&1 && sleep 3
 sudo killall $COIN_DAEMON > /dev/null 2>&1
-sudo systemctl stop zelbench > /dev/null 2>&1 && sleep 3
-sudo killall $BENCH_DAEMON > /dev/null 2>&1
 sleep 2
-zelcashd
+$COIN_DAEMON
