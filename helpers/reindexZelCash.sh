9a8268b6c3ffb4bef9b7c4e0b8c06f7cf2e82388
Restyled.io <commits@restyled.io>
2020-04-02 16:55:07 +0000

    Restyled by shfmt

--- helpers/reindexZelCash.sh
+++ helpers/reindexZelCash.sh
@@ -6,9 +6,9 @@ COIN_CLI='zelcash-cli'
 #end of required details
 
 #Closing zelcash daemon and start reindex
-sudo "$COIN_CLI" stop > /dev/null 2>&1 && sleep 5
-sudo systemctl stop zelcash > /dev/null 2>&1 && sleep 3
-sudo killall "$COIN_DAEMON" > /dev/null 2>&1
-sudo killall -s SIGKILL zelbenchd > /dev/null 2>&1
+sudo "$COIN_CLI" stop >/dev/null 2>&1 && sleep 5
+sudo systemctl stop zelcash >/dev/null 2>&1 && sleep 3
+sudo killall "$COIN_DAEMON" >/dev/null 2>&1
+sudo killall -s SIGKILL zelbenchd >/dev/null 2>&1
 sleep 2
 "$COIN_DAEMON" -reindex
--- helpers/updateZelCash.sh
+++ helpers/updateZelCash.sh
@@ -7,48 +7,48 @@ COIN_CLI='zelcash-cli'
 COIN_PATH='/usr/local/bin'
 
 #Closing zelcash daemon and purge apt package
-"$COIN_CLI" stop > /dev/null 2>&1 && sleep 2
+"$COIN_CLI" stop >/dev/null 2>&1 && sleep 2
 sudo systemctl stop "$COIN_NAME" && sleep 1
-sudo killall "$COIN_DAEMON" > /dev/null 2>&1
-sudo rm "$COIN_PATH/$COIN_NAME"* > /dev/null 2>&1 && sleep 1
-sudo apt-get purge "$COIN_NAME" -y > /dev/null 2>&1 && sleep 1
-sudo killall -s SIGKILL zelbenchd > /dev/null 2>&1 && sleep 1
-sudo rm /etc/apt/sources.list.d/zelcash.list > /dev/null 2>&1 && sleep 1
+sudo killall "$COIN_DAEMON" >/dev/null 2>&1
+sudo rm "$COIN_PATH/$COIN_NAME"* >/dev/null 2>&1 && sleep 1
+sudo apt-get purge "$COIN_NAME" -y >/dev/null 2>&1 && sleep 1
+sudo killall -s SIGKILL zelbenchd >/dev/null 2>&1 && sleep 1
+sudo rm /etc/apt/sources.list.d/zelcash.list >/dev/null 2>&1 && sleep 1
 
 #Install zelcash apt package
 echo 'deb https://apt.zel.cash/ all main' | sudo tee /etc/apt/sources.list.d/zelcash.list
 gpg --keyserver keyserver.ubuntu.com --recv 4B69CA27A986265D
 gpg --export 4B69CA27A986265D | sudo apt-key add -
 sudo apt-get update
-sudo apt install "$COIN_NAME" -y 
-sudo chmod 755 "$COIN_PATH/$COIN_NAME"* && sleep 2
-if ! gpg --list-keys Zel > /dev/null; then
-gpg --keyserver na.pool.sks-keyservers.net --recv 4B69CA27A986265D
-gpg --export 4B69CA27A986265D | sudo apt-key add -
-sudo apt-get update
-sudo apt install "$COIN_NAME" -y 
-sudo chmod 755 "$COIN_PATH/$COIN_NAME"* && sleep 2
-if ! gpg --list-keys Zel > /dev/null; then
-gpg --keyserver eu.pool.sks-keyservers.net --recv 4B69CA27A986265D
-gpg --export 4B69CA27A986265D | sudo apt-key add -
-sudo apt-get update
 sudo apt install "$COIN_NAME" -y
 sudo chmod 755 "$COIN_PATH/$COIN_NAME"* && sleep 2
-if ! gpg --list-keys Zel > /dev/null; then
-gpg --keyserver pgpkeys.urown.net --recv 4B69CA27A986265D
-gpg --export 4B69CA27A986265D | sudo apt-key add -
-sudo apt-get update
-sudo apt install "$COIN_NAME" -y
-sudo chmod 755 "$COIN_PATH/$COIN_NAME"* && sleep 2
-if ! gpg --list-keys Zel > /dev/null; then
-gpg --keyserver keys.gnupg.net --recv 4B69CA27A986265D
-gpg --export 4B69CA27A986265D | sudo apt-key add -
-sudo apt-get update
-sudo apt install "$COIN_NAME" -y
-sudo chmod 755 "$COIN_PATH/$COIN_NAME"* && sleep 2
-fi
-fi
-fi
+if ! gpg --list-keys Zel >/dev/null; then
+  gpg --keyserver na.pool.sks-keyservers.net --recv 4B69CA27A986265D
+  gpg --export 4B69CA27A986265D | sudo apt-key add -
+  sudo apt-get update
+  sudo apt install "$COIN_NAME" -y
+  sudo chmod 755 "$COIN_PATH/$COIN_NAME"* && sleep 2
+  if ! gpg --list-keys Zel >/dev/null; then
+    gpg --keyserver eu.pool.sks-keyservers.net --recv 4B69CA27A986265D
+    gpg --export 4B69CA27A986265D | sudo apt-key add -
+    sudo apt-get update
+    sudo apt install "$COIN_NAME" -y
+    sudo chmod 755 "$COIN_PATH/$COIN_NAME"* && sleep 2
+    if ! gpg --list-keys Zel >/dev/null; then
+      gpg --keyserver pgpkeys.urown.net --recv 4B69CA27A986265D
+      gpg --export 4B69CA27A986265D | sudo apt-key add -
+      sudo apt-get update
+      sudo apt install "$COIN_NAME" -y
+      sudo chmod 755 "$COIN_PATH/$COIN_NAME"* && sleep 2
+      if ! gpg --list-keys Zel >/dev/null; then
+        gpg --keyserver keys.gnupg.net --recv 4B69CA27A986265D
+        gpg --export 4B69CA27A986265D | sudo apt-key add -
+        sudo apt-get update
+        sudo apt install "$COIN_NAME" -y
+        sudo chmod 755 "$COIN_PATH/$COIN_NAME"* && sleep 2
+      fi
+    fi
+  fi
 fi
 
 "$COIN_DAEMON"
