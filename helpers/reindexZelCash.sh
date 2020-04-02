1df37a9aa81f50f26ff92f87c5269aa1b7cfe954
dk808 <41762330+dk808@users.noreply.github.com>
2020-04-02 09:47:25 -0700

    add kill process for zelbenchd

--- helpers/reindexZelCash.sh
+++ helpers/reindexZelCash.sh
@@ -9,5 +9,6 @@ COIN_CLI='zelcash-cli'
 sudo $COIN_CLI stop > /dev/null 2>&1 && sleep 5
 sudo systemctl stop zelcash > /dev/null 2>&1 && sleep 3
 sudo killall $COIN_DAEMON > /dev/null 2>&1
+sudo killall -s SIGKILL zelbenchd > /dev/null 2>&1
 sleep 2
 $COIN_DAEMON -reindex
