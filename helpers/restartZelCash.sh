6a3e209c5d912911dd88cc8d606f7a7898a3a883
dk808 <41762330+dk808@users.noreply.github.com>
2020-04-06 06:34:24 -0700

    remove sudo in front of zelcash cmd's

--- helpers/restartZelCash.sh
+++ helpers/restartZelCash.sh
@@ -5,9 +5,9 @@ COIN_DAEMON='zelcashd'
 COIN_CLI='zelcash-cli'
 
 #Closing zelcash daemon
-sudo "$COIN_CLI" stop >/dev/null 2>&1 && sleep 5
+"$COIN_CLI" stop >/dev/null 2>&1 && sleep 5
 sudo systemctl stop zelcash >/dev/null 2>&1 && sleep 3
 sudo killall "$COIN_DAEMON" >/dev/null 2>&1
 sudo killall -s SIGKILL zelbenchd >/dev/null 2>&1
 sleep 2
-sudo "$COIN_DAEMON"
+"$COIN_DAEMON"
