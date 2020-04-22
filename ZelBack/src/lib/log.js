772ad1af65a2c630cacd233bee94020375de8777
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2020-04-22 15:02:47 +0200

    fix typo + log + better time support

--- ZelBack/src/lib/log.js
+++ ZelBack/src/lib/log.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const moment = require('moment');
 
 const homeDirPath = path.join(__dirname, '../../../../');
 
@@ -14,9 +15,10 @@ function getFilesizeInBytes(filename) {
 }
 
 function error(...args) {
+  
   try {
     console.error(...args);
-    // write to file
+    let error;
     const datadir = `${homeDirPath}zelflux`;
     const filepath = `${datadir}/error.log`;
     const size = getFilesizeInBytes(filepath);
@@ -24,13 +26,27 @@ function error(...args) {
     if (size > (25 * 1000 * 1000)) { // 25MB
       flag = 'w'; // rewrite file
     }
+    
+   const date = moment.utc().format('YYYY-MM-DD HH:mm:ss');
+   const stillUtc = moment.utc(date).toDate();
+   const local = moment(stillUtc).local().format('YYYY-MM-DD HH:mm:ss');
+    
+   if ( `${[...args]}` == "[object Object]") {
+    error = JSON.parse(JSON.stringify(...args));
+    error = error.message; 
+   } else {
+     error = args;
+   }
+    
     const stream = fs.createWriteStream(filepath, { flags: flag });
-    stream.write(`${new Date().toISOString()}          ${[...args]}\n`);
+    stream.write(`${new Date().toISOString()} => ${error} / Local Time: ${local}\n`);
     stream.end();
-  } catch (err) {
+    
+    } catch (err) {
     console.error('This shall not have happened');
     console.error(err);
   }
+  
 }
 
 module.exports = {
