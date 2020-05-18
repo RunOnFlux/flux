0632ff84f9cc33d090af68bd6d9d44bbcc73cf18
dk808 <41762330+dk808@users.noreply.github.com>
2020-05-18 04:41:30 -0700

    Update helpers scripts to start service if detected

--- helpers/reindexZelCash.sh
+++ helpers/reindexZelCash.sh
@@ -11,9 +11,12 @@ PATH=$PATH:"$COIN_PATH"
 export PATH
 
 #Closing zelcash daemon and start reindex
-"$COIN_CLI" stop >/dev/null 2>&1 && sleep 5
 sudo systemctl stop zelcash >/dev/null 2>&1 && sleep 3
+"$COIN_CLI" stop >/dev/null 2>&1 && sleep 3
 sudo killall "$COIN_DAEMON" >/dev/null 2>&1
 sudo killall -s SIGKILL zelbenchd >/dev/null 2>&1
 sleep 2
-"$COIN_DAEMON" -reindex
+"$COIN_DAEMON" -reindex && sleep 60
+if sudo systemctl list-units --full -all | grep -o 'zelcash.service' | head -n1 >/dev/null; then
+  "$COIN_CLI" stop >/dev/null 2>&1 && sleep 5 && sudo systemctl start zelcash
+fi
--- helpers/updateZelCash.sh
+++ helpers/updateZelCash.sh
@@ -11,8 +11,8 @@ PATH=$PATH:"$COIN_PATH"
 export PATH
 
 #Closing zelcash daemon and purge apt package
-"$COIN_CLI" stop >/dev/null 2>&1 && sleep 2
-sudo systemctl stop "$COIN_NAME" && sleep 1
+sudo systemctl stop "$COIN_NAME" && sleep 3
+"$COIN_CLI" stop >/dev/null 2>&1 && sleep 3
 sudo killall "$COIN_DAEMON" >/dev/null 2>&1
 sudo rm "$COIN_PATH/$COIN_NAME"* >/dev/null 2>&1 && sleep 1
 sudo apt-get purge "$COIN_NAME" -y >/dev/null 2>&1 && sleep 1
@@ -54,5 +54,8 @@ if ! gpg --list-keys Zel >/dev/null; then
     fi
   fi
 fi
-
-"$COIN_DAEMON"
+if sudo systemctl list-units --full -all | grep -o 'zelcash.service' | head -n1 >/dev/null; then
+  sudo systemctl start zelcash
+else
+  "$COIN_DAEMON"
+fi
