c91b900607e30f74149e071b543fd910f7cbbc5e
TheTrunk <tadeas.kmenta@zel.cash>
2020-07-19 21:23:27 +0200

    remove test

--- test.js
+++
@@ -1,6 +0,0 @@
-const runningAppsNames = ['FoldingAtHome', 'dibi-UND', 'PacMan', 'SuperMario'];
-const installedAppsNames = ['FoldingAtHome', 'dibi-UND', 'PacMan', 'abc', 'SuperMario'];
-const runningSet = new Set(runningAppsNames);
-const abc = installedAppsNames.filter((installedApp) => !runningSet.has(installedApp));
-
-console.log(abc);
