cc7eb5ad212880c6744dc1f42c1fd97d27a38b40
TheTrunk <tadeas.kmenta@zel.cash>
2020-07-19 12:54:08 +0200

    add specifications about app lasting/removal

--- ZelBack/config/default.js
+++ ZelBack/config/default.js
@@ -68,6 +68,8 @@ module.exports = {
       probability: 100, // 1 to 100
       delay: 30, // in seconds
     },
+    blocksLasting: 22000, // registered app will live for 22000 of blocks 44000 minutes ~= 1 month
+    // every 100 blocks we run a check that deletes apps specifications and stops/removes the application from existence if it has been lastly updated more than 22k blocks ago
   },
   lockedSystemResources: {
     cpu: 10, // 1 cpu core
