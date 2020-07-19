85b818ec044eaccd6a4df56d43defda83bc19b41
Restyled.io <commits@restyled.io>
2020-07-19 18:31:32 +0000

    Restyled by clang-format

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
-const runningAppsNames = ['FoldingAtHome', 'dibi-UND', 'PacMan', 'SuperMario'];
-const installedAppsNames = ['FoldingAtHome', 'dibi-UND', 'PacMan', 'abc', 'SuperMario'];
+const runningAppsNames =
+    [ 'FoldingAtHome', 'dibi-UND', 'PacMan', 'SuperMario' ];
+const installedAppsNames =
+    [ 'FoldingAtHome', 'dibi-UND', 'PacMan', 'abc', 'SuperMario' ];
 const runningSet = new Set(runningAppsNames);
-const abc = installedAppsNames.filter((installedApp) => !runningSet.has(installedApp));
+const abc =
+    installedAppsNames.filter((installedApp) => !runningSet.has(installedApp));
 
 console.log(abc);
