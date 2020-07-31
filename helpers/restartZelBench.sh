6be2f714b5ec6e3812e77f6584c416c8af566931
TheTrunk <tadeas.kmenta@zel.cash>
2020-07-31 16:12:40 +0200

    fix restart zelbench script

--- helpers/restartZelBench.sh
+++ helpers/restartZelBench.sh
@@ -3,7 +3,6 @@
 #information
 COIN_NAME='zelbench'
 COIN_DAEMON='zelbenchd'
-COIN_DAEMON_START='zelbenchd -daemon'
 COIN_CLI='zelbench-cli'
 COIN_PATH='/usr/local/bin'
 #end of required details
@@ -22,6 +21,6 @@ sleep 2
 if sudo systemctl list-units --full --no-legend --no-pager --plain --all --type service "$COIN_NAME.service" | grep -Foq "$COIN_NAME.service"; then
   sudo systemctl start "$COIN_NAME"
 else
-  "$COIN_DAEMON_START"
+  "$COIN_DAEMON" -daemon
 fi
 
