38970b801a932e14a39f528da2edaebda9c4a33e
Restyled.io <commits@restyled.io>
2020-10-29 11:55:08 +0000

    Restyled by prettier

--- ZelBack/src/services/serviceManager.js
+++ ZelBack/src/services/serviceManager.js
@@ -15,44 +15,47 @@ async function startFluxFunctions() {
     const db = serviceHelper.databaseConnection();
     const database = db.db(config.database.local.database);
     await serviceHelper
-        .dropCollection(database,
-                        config.database.local.collections.activeLoginPhrases)
-        .catch((error) => {
-          if (error.message !== 'ns not found') {
-            log.error(error);
-          }
-        });
+      .dropCollection(
+        database,
+        config.database.local.collections.activeLoginPhrases
+      )
+      .catch((error) => {
+        if (error.message !== 'ns not found') {
+          log.error(error);
+        }
+      });
     await serviceHelper
-        .dropCollection(database,
-                        config.database.local.collections.activeSignatures)
-        .catch((error) => {
-          if (error.message !== 'ns not found') {
-            log.error(error);
-          }
-        });
+      .dropCollection(
+        database,
+        config.database.local.collections.activeSignatures
+      )
+      .catch((error) => {
+        if (error.message !== 'ns not found') {
+          log.error(error);
+        }
+      });
     await database
-        .collection(config.database.local.collections.activeLoginPhrases)
-        .createIndex({createdAt : 1}, {expireAfterSeconds : 900});
+      .collection(config.database.local.collections.activeLoginPhrases)
+      .createIndex({ createdAt: 1 }, { expireAfterSeconds: 900 });
     await database
-        .collection(config.database.local.collections.activeSignatures)
-        .createIndex({createdAt : 1}, {expireAfterSeconds : 900});
+      .collection(config.database.local.collections.activeSignatures)
+      .createIndex({ createdAt: 1 }, { expireAfterSeconds: 900 });
     log.info('Local database prepared');
     log.info('Preparing temporary database...');
     // no need to drop temporary messages
     const databaseTemp = db.db(config.database.zelappsglobal.database);
     await databaseTemp
-        .collection(
-            config.database.zelappsglobal.collections.zelappsTemporaryMessages)
-        .createIndex(
-            {receivedAt : 1},
-            {expireAfterSeconds : 3600}); // todo longer time? dropIndexes()
+      .collection(
+        config.database.zelappsglobal.collections.zelappsTemporaryMessages
+      )
+      .createIndex({ receivedAt: 1 }, { expireAfterSeconds: 3600 }); // todo longer time? dropIndexes()
     log.info('Temporary database prepared');
     log.info('Preparing zelapps locations');
     // more than 1 hour. Meaning we have not received status message for a long
     // time. So that node is no longer on a network or app is down.
     await databaseTemp
-        .collection(config.database.zelappsglobal.collections.zelappsLocations)
-        .createIndex({broadcastedAt : 1}, {expireAfterSeconds : 3900});
+      .collection(config.database.zelappsglobal.collections.zelappsLocations)
+      .createIndex({ broadcastedAt: 1 }, { expireAfterSeconds: 3900 });
     log.info('ZelApps locations prepared');
     zelfluxCommunication.adjustFirewall();
     zelfluxCommunication.fluxDisovery();
@@ -60,15 +63,17 @@ async function startFluxFunctions() {
     zelfluxCommunication.keepConnectionsAlive();
     zelfluxCommunication.keepIncomingConnectionsAlive();
     zelfluxCommunication.checkDeterministicNodesCollisions();
-    setInterval(
-        () => { zelfluxCommunication.checkDeterministicNodesCollisions(); },
-        60000);
+    setInterval(() => {
+      zelfluxCommunication.checkDeterministicNodesCollisions();
+    }, 60000);
     log.info('Flux checks operational');
     explorerService.initiateBlockProcessor(true, true);
-    setInterval(() => { // every 8 mins (4 blocks)
+    setInterval(() => {
+      // every 8 mins (4 blocks)
       zelappsService.continuousZelAppHashesCheck();
     }, 8 * 60 * 1000);
-    setInterval(() => { // every 4 mins (2 blocks)
+    setInterval(() => {
+      // every 4 mins (2 blocks)
       zelappsService.checkAndNotifyPeersOfRunningApps();
     }, 4 * 60 * 1000);
     log.info('Flux Block Processing Service started');
@@ -79,7 +84,9 @@ async function startFluxFunctions() {
     }, 16 * 60 * 1000);
   } catch (e) {
     log.error(e);
-    setTimeout(() => { startFluxFunctions(); }, 15000);
+    setTimeout(() => {
+      startFluxFunctions();
+    }, 15000);
   }
 }
 
