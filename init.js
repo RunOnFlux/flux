235dcd8e853afb887834c48f0ed5787f5b1d2350
Restyled.io <commits@restyled.io>
2020-12-25 02:17:50 +0000

    Restyled by clang-format

--- init.js
+++ init.js
@@ -5,21 +5,23 @@ const path = './config/userconfig.js';
 
 const goodchars = /^[1-9a-km-zA-HJ-NP-Z]+$/;
 if (fs.existsSync(path)) {
-  console.log('Configuration file found. You can change your configuration in ./config/userconfig.js');
+  console.log(
+      'Configuration file found. You can change your configuration in ./config/userconfig.js');
   console.log('Starting Flux...');
   return;
 }
 
 const questions = [
   {
-    type: 'input',
-    name: 'ipaddr',
-    message: 'What is your ZelNodes IP address?',
+    type : 'input',
+    name : 'ipaddr',
+    message : 'What is your ZelNodes IP address?',
   },
   {
-    type: 'input',
-    name: 'zelid',
-    message: 'Flux uses Zel ID system for authentication and verification purposes. What is your Zel ID?',
+    type : 'input',
+    name : 'zelid',
+    message :
+        'Flux uses Zel ID system for authentication and verification purposes. What is your Zel ID?',
   },
 ];
 
@@ -27,11 +29,13 @@ function showQuestions() {
   inquirer.prompt(questions).then((answers) => {
     console.log(`IP address: ${answers.ipaddr}`);
     console.log(`Zel ID: ${answers.zelid}`);
-    if (answers.ipaddr.length < 5 || (answers.ipaddr.indexOf('.') === -1 && answers.ipaddr.indexOf(':') === -1)) {
+    if (answers.ipaddr.length < 5 || (answers.ipaddr.indexOf('.') === -1 &&
+                                      answers.ipaddr.indexOf(':') === -1)) {
       console.log('IP address is NOT valid!');
       return showQuestions();
     }
-    if (!goodchars.test(answers.zelid) || answers.zelid[0] !== '1' || answers.zelid.length > 34 || answers.zelid.length < 25) {
+    if (!goodchars.test(answers.zelid) || answers.zelid[0] !== '1' ||
+        answers.zelid.length > 34 || answers.zelid.length < 25) {
       console.log('zelID is NOT valid!');
       return showQuestions();
     }
@@ -61,7 +65,8 @@ function showQuestions() {
       userconfig.write(dataToWrite);
       userconfig.end();
     });
-    console.log('Configuration successful. Values saved and they can be changed in ./config/userconfig.js');
+    console.log(
+        'Configuration successful. Values saved and they can be changed in ./config/userconfig.js');
     console.log('Starting Flux...');
     return 0;
   });
