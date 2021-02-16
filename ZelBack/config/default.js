917d0844917a8893c050e53fd5108cfb72b79fa2
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2021-02-16 11:04:52 +0800

    redeploy probability

--- ZelBack/config/default.js
+++ ZelBack/config/default.js
@@ -88,7 +88,7 @@ module.exports = {
       delay: 300,
     },
     redeploy: {
-      probability: 100, // 1%
+      probability: 2, // 50%
       delay: 30,
     },
     blocksLasting: 22000, // registered app will live for 22000 of blocks 44000 minutes ~= 1 month
