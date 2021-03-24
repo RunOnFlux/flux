382c5c29765d2d34bd981983910253565548ce90
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-03-24 20:19:19 +0100

    update for flux

--- helpers/restartBenchmark.sh
+++ helpers/restartBenchmark.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #information
-COIN_NAME='zelbench'
-COIN_DAEMON='zelbenchd'
-COIN_CLI='zelbench-cli'
+COIN_NAME='flux'
+COIN_DAEMON='fluxd'
+BENCH_DAEMON='fluxbenchd'
+COIN_CLI='fluxbench-cli'
 COIN_PATH='/usr/local/bin'
 #end of required details
 
@@ -15,7 +16,7 @@ export PATH
 sudo systemctl stop "$COIN_NAME" >/dev/null 2>&1 && sleep 3
 "$COIN_CLI" stop >/dev/null 2>&1 && sleep 3
 sudo killall "$COIN_DAEMON" >/dev/null 2>&1
-sudo killall -s SIGKILL "$COIN_DAEMON" >/dev/null 2>&1
+sudo killall -s SIGKILL "$BENCH_DAEMON" >/dev/null 2>&1
 sleep 2
 
 if sudo systemctl list-units --full --no-legend --no-pager --plain --all --type service "$COIN_NAME.service" | grep -Foq "$COIN_NAME.service"; then
