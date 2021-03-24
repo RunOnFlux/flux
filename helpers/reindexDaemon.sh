ae6aaf8c163e3b9ab0d57acd91f484ecaeec4a28
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-03-24 20:21:34 +0100

    update for flux

--- helpers/reindexDaemon.sh
+++ helpers/reindexDaemon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #information
-COIN_NAME='zelcash'
-COIN_DAEMON='zelcashd'
-COIN_CLI='zelcash-cli'
+COIN_NAME='flux'
+COIN_DAEMON='fluxd'
+COIN_CLI='flux-cli'
 COIN_PATH='/usr/local/bin'
 #end of required details
 
@@ -15,7 +15,7 @@ export PATH
 sudo systemctl stop "$COIN_NAME" >/dev/null 2>&1 && sleep 3
 "$COIN_CLI" stop >/dev/null 2>&1 && sleep 3
 sudo killall "$COIN_DAEMON" >/dev/null 2>&1
-sudo killall -s SIGKILL zelbenchd >/dev/null 2>&1
+sudo killall -s SIGKILL fluxbenchd >/dev/null 2>&1
 sleep 2
 
 "$COIN_DAEMON" -reindex && sleep 60
@@ -23,7 +23,7 @@ if sudo systemctl list-units --full --no-legend --no-pager --plain --all --type 
   sudo systemctl stop "$COIN_NAME" >/dev/null 2>&1 && sleep 3
   "$COIN_CLI" stop >/dev/null 2>&1 && sleep 3
   sudo killall "$COIN_DAEMON" >/dev/null 2>&1
-  sudo killall -s SIGKILL zelbenchd >/dev/null 2>&1
+  sudo killall -s SIGKILL fluxbenchd >/dev/null 2>&1
   sleep 2
   sudo systemctl start "$COIN_NAME"
 fi
