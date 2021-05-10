4b1e205c6174eaf7d1eed80943018eca6fea7ee8
dennis <dennis@sygn.net>
2021-05-10 11:51:48 +0300

    refactor and cleanup store (not tested!)

--- ZelFront/src/store.js
+++ ZelFront/src/store.js
@@ -1,114 +1,89 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-const config = require('../../ZelBack/config/default');
-const userconfig = require('../../config/userconfig');
+const config			= require('../../ZelBack/config/default');
+const userconfig		= require('../../config/userconfig');
+const ALL_SECTIONS	= [ 'adminSection', 'appsSection', 'benchmarkSection', 'daemonSection', 'dashboardSection', 'explorerSection', 'nodeSection' ];
 
 Vue.use(Vuex);
 
+
+const nullSections = (state) => {
+	for (const section of ALL_SECTIONS ) state[section] = null;
+};
+
+
+const setSection = (sectionId, state, section) => {
+	nullSections( state );
+	state[ sectionId ] = section;
+};
+
+
+
 export default new Vuex.Store({
-  strict: true,
+
+  strict: process.env.NODE_ENV !== 'production',
+
   state: {
     userconfig: {
-      zelid: userconfig.initial.zelid,
-      externalip: userconfig.initial.ipaddress,
-      cruxid: userconfig.initial.cruxid,
-      kadena: userconfig.initial.kadena,
-      testnet: userconfig.initial.testnet,
+      cruxid		: userconfig.initial.cruxid,
+      externalip	: userconfig.initial.ipaddress,
+      kadena		: userconfig.initial.kadena,
+      testnet		: userconfig.initial.testnet,
+      zelid			: userconfig.initial.zelid,
     },
-    config: {
-      apiPort: config.server.apiport,
-      fluxTeamZelId: config.fluxTeamZelId,
+
+	 config: {
+      apiPort			: config.server.apiport,
+      fluxTeamZelId	: config.fluxTeamZelId,
     },
-    privilage: 'none', // user, admin, fluxteam
-    daemonSection: 'welcomeinfo',
-    benchmarkSection: null,
-    nodeSection: null,
-    adminSection: null,
-    appsSection: null,
-    explorerSection: null,
-    dashboardSection: null,
-    fluxVersion: '',
+
+	 privilage			: 'none',	// user, admin, fluxteam
+    adminSection		: null,
+    appsSection		: null,
+    benchmarkSection	: null,
+    daemonSection		: 'welcomeinfo',
+    dashboardSection	: null,
+    explorerSection	: null,
+    fluxVersion		: '',
+    nodeSection		: null,
   },
-  getters: {
+
+	getters: {
+	},
+
+	mutations: {
+		setPrivilage(state, privilage) {
+			state.privilage = privilage;
+		},
+
+		setAdminSection(state, section) {
+			setSection( 'adminSection', state, section );
+		},
+		setAppsSection(state, section) {
+			setSection( 'appsSection', state, section );
+		},
+		setBenchmarkSection(state, section) {
+			setSection( 'benchmarkSection', state, section );
+		},
+		setDaemonSection(state, section) {
+			setSection( 'daemonSection', state, section );
+		},
+		setDashboardSection(state, section) {
+			setSection( 'dashboardSection', state, section );
+		},
+		setExplorerSection(state, section) {
+			setSection( 'explorerSection', state, section );
+		},
+		setNodeSection(state, section) {
+			setSection( 'nodeSection', state, section );
+		},
+
+		setFluxVersion(state, version) {
+			state.fluxVersion = version;
+		},
   },
-  mutations: {
-    setPrivilage(state, privilage) {
-      state.privilage = privilage;
-    },
-    setDaemonSection(state, section) {
-      state.daemonSection = section;
-      // we always want to reset the other sections to null state
-      state.benchmarkSection = null;
-      state.nodeSection = null;
-      state.adminSection = null;
-      state.appsSection = null;
-      state.explorerSection = null;
-      state.dashboardSection = null;
-    },
-    setBenchmarkSection(state, section) {
-      state.benchmarkSection = section;
-      // we always want to reset the other sections to null state
-      state.daemonSection = null;
-      state.nodeSection = null;
-      state.adminSection = null;
-      state.appsSection = null;
-      state.explorerSection = null;
-      state.dashboardSection = null;
-    },
-    setNodeSection(state, section) {
-      state.nodeSection = section;
-      // we always want to reset the other sections to null state
-      state.daemonSection = null;
-      state.benchmarkSection = null;
-      state.adminSection = null;
-      state.appsSection = null;
-      state.explorerSection = null;
-      state.dashboardSection = null;
-    },
-    setAdminSection(state, section) {
-      state.adminSection = section;
-      // we always want to reset the other sections to null state
-      state.daemonSection = null;
-      state.benchmarkSection = null;
-      state.nodeSection = null;
-      state.appsSection = null;
-      state.explorerSection = null;
-      state.dashboardSection = null;
-    },
-    setAppsSection(state, section) {
-      state.appsSection = section;
-      // we always want to reset the other sections to null state
-      state.daemonSection = null;
-      state.benchmarkSection = null;
-      state.nodeSection = null;
-      state.adminSection = null;
-      state.explorerSection = null;
-      state.dashboardSection = null;
-    },
-    setExplorerSection(state, section) {
-      state.explorerSection = section;
-      // we always want to reset the other sections to null state
-      state.daemonSection = null;
-      state.benchmarkSection = null;
-      state.nodeSection = null;
-      state.adminSection = null;
-      state.appsSection = null;
-      state.dashboardSection = null;
-    },
-    setDashboardSection(state, section) {
-      state.dashboardSection = section;
-      // we always want to reset the other sections to null state
-      state.daemonSection = null;
-      state.benchmarkSection = null;
-      state.nodeSection = null;
-      state.adminSection = null;
-      state.appsSection = null;
-      state.explorerSection = null;
-    },
-    setFluxVersion(state, version) {
-      state.fluxVersion = version;
-    },
-  },
+
   actions: {},
+
 });
