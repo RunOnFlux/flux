9ead0da3746c8a2a79653ffe161f5f8a39678754
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-05-19 20:58:15 +0200

    added -y for update

--- helpers/updateDaemon.sh
+++ helpers/updateDaemon.sh
@@ -49,7 +49,7 @@ if [[ "$(lsb_release -cs)" == "xenial" ]]; then
          gpg --export 4B69CA27A986265D | sudo apt-key add - > /dev/null 2>&1   
      fi
 
-    sudo apt-get update
+    sudo apt-get update -y
     sudo apt-get install "$COIN_NAME" -y
     sudo chmod 755 "$COIN_PATH/"* && sleep 2
           
@@ -60,7 +60,7 @@ else
   if gpg -k --keyring /usr/share/keyrings/flux-archive-keyring.gpg Zel > /dev/null 2>&1; then
    
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/flux-archive-keyring.gpg] https://apt.runonflux.io/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/flux.list > /dev/null 2>&1
-    sudo apt-get update
+    sudo apt-get update -y
     sudo apt-get install "$COIN_NAME" -y
     sudo chmod 755 "$COIN_PATH/"* && sleep 2
 
@@ -85,7 +85,7 @@ else
         gpg --no-default-keyring --keyring /usr/share/keyrings/flux-archive-keyring.gpg --keyserver hkp://keys.gnupg.net:80 --recv-keys 4B69CA27A986265D > /dev/null 2>&1
      fi
 
-    sudo apt-get update
+    sudo apt-get update -y
     sudo apt-get install "$COIN_NAME" -y
     sudo chmod 755 "$COIN_PATH/"* && sleep 2
 
