33bbb5c77f41195d85aebb5768a205bbe2d16ae6
SyGem Limited <simon@sygem.com>
2021-07-08 20:47:41 +0100

    Added an overlay spinner to "Flux Network"

--- ZelFront/src/views/flux/FluxNetwork.vue
+++ ZelFront/src/views/flux/FluxNetwork.vue
@@ -4,192 +4,204 @@
       active
       title="Outgoing"
     >
-      <b-card>
-        <b-row>
-          <b-col
-            md="4"
-            sm="4"
-            class="my-1"
-          >
-            <b-form-group class="mb-0">
-              <label class="d-inline-block text-left mr-50">Per page</label>
-              <b-form-select
-                id="perPageSelect"
-                v-model="config.outgoing.perPage"
-                size="sm"
-                :options="config.outgoing.pageOptions"
-                class="w-50"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col
-            md="8"
-            class="my-1"
-          >
-            <b-form-group
-              label="Filter"
-              label-cols-sm="1"
-              label-align-sm="right"
-              label-for="filterInput"
-              class="mb-0"
+      <b-overlay
+        :show="config.outgoing.connectedPeers.length === 0"
+        variant="transparent"
+        blur="5px"
+      >
+        <b-card>
+          <b-row>
+            <b-col
+              md="4"
+              sm="4"
+              class="my-1"
             >
-              <b-input-group size="sm">
-                <b-form-input
-                  id="filterInput"
-                  v-model="config.outgoing.filter"
-                  type="search"
-                  placeholder="Type to Search"
-                />
-                <b-input-group-append>
-                  <b-button
-                    :disabled="!config.outgoing.filter"
-                    @click="config.outgoing.filter = ''"
-                  >
-                    Clear
-                  </b-button>
-                </b-input-group-append>
-              </b-input-group>
-            </b-form-group>
-          </b-col>
-
-          <b-col cols="12">
-            <b-table
-              class="fluxnetwork-outgoing-table"
-              striped
-              hover
-              responsive
-              :per-page="config.outgoing.perPage"
-              :current-page="config.outgoing.currentPage"
-              :items="config.outgoing.connectedPeers"
-              :fields="config.outgoing.fields"
-              :sort-by.sync="config.outgoing.sortBy"
-              :sort-desc.sync="config.outgoing.sortDesc"
-              :sort-direction="config.outgoing.sortDirection"
-              :filter="config.outgoing.filter"
-              :filter-included-fields="config.outgoing.filterOn"
-              show-empty
-              empty-text="No Connected Nodes"
-              @filtered="onFilteredOutgoing"
-            >
-              <template #cell(disconnect)="row">
-                <b-button
+              <b-form-group class="mb-0">
+                <label class="d-inline-block text-left mr-50">Per page</label>
+                <b-form-select
+                  id="perPageSelect"
+                  v-model="config.outgoing.perPage"
                   size="sm"
-                  class="mr-0"
-                  variant="danger"
-                  @click="disconnectPeer(row)"
-                >
-                  Disconnect
-                </b-button>
-              </template>
-            </b-table>
-          </b-col>
+                  :options="config.outgoing.pageOptions"
+                  class="w-50"
+                />
+              </b-form-group>
+            </b-col>
+            <b-col
+              md="8"
+              class="my-1"
+            >
+              <b-form-group
+                label="Filter"
+                label-cols-sm="1"
+                label-align-sm="right"
+                label-for="filterInput"
+                class="mb-0"
+              >
+                <b-input-group size="sm">
+                  <b-form-input
+                    id="filterInput"
+                    v-model="config.outgoing.filter"
+                    type="search"
+                    placeholder="Type to Search"
+                  />
+                  <b-input-group-append>
+                    <b-button
+                      :disabled="!config.outgoing.filter"
+                      @click="config.outgoing.filter = ''"
+                    >
+                      Clear
+                    </b-button>
+                  </b-input-group-append>
+                </b-input-group>
+              </b-form-group>
+            </b-col>
 
-          <b-col cols="12">
-            <b-pagination
-              v-model="config.outgoing.currentPage"
-              :total-rows="config.outgoing.totalRows"
-              :per-page="config.outgoing.perPage"
-              align="center"
-              size="sm"
-              class="my-0"
-            />
-          </b-col>
-        </b-row>
-      </b-card>
+            <b-col cols="12">
+              <b-table
+                class="fluxnetwork-outgoing-table"
+                striped
+                hover
+                responsive
+                :per-page="config.outgoing.perPage"
+                :current-page="config.outgoing.currentPage"
+                :items="config.outgoing.connectedPeers"
+                :fields="config.outgoing.fields"
+                :sort-by.sync="config.outgoing.sortBy"
+                :sort-desc.sync="config.outgoing.sortDesc"
+                :sort-direction="config.outgoing.sortDirection"
+                :filter="config.outgoing.filter"
+                :filter-included-fields="config.outgoing.filterOn"
+                show-empty
+                empty-text="No Connected Nodes"
+                @filtered="onFilteredOutgoing"
+              >
+                <template #cell(disconnect)="row">
+                  <b-button
+                    size="sm"
+                    class="mr-0"
+                    variant="danger"
+                    @click="disconnectPeer(row)"
+                  >
+                    Disconnect
+                  </b-button>
+                </template>
+              </b-table>
+            </b-col>
+
+            <b-col cols="12">
+              <b-pagination
+                v-model="config.outgoing.currentPage"
+                :total-rows="config.outgoing.totalRows"
+                :per-page="config.outgoing.perPage"
+                align="center"
+                size="sm"
+                class="my-0"
+              />
+            </b-col>
+          </b-row>
+        </b-card>
+      </b-overlay>
     </b-tab>
     <b-tab title="Incoming">
-      <b-card>
-        <b-row>
-          <b-col
-            md="4"
-            sm="4"
-            class="my-1"
-          >
-            <b-form-group class="mb-0">
-              <label class="d-inline-block text-left mr-50">Per page</label>
-              <b-form-select
-                id="perPageSelect"
-                v-model="config.incoming.perPage"
-                size="sm"
-                :options="config.incoming.pageOptions"
-                class="w-50"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col
-            md="8"
-            class="my-1"
-          >
-            <b-form-group
-              label="Filter"
-              label-cols-sm="1"
-              label-align-sm="right"
-              label-for="filterInput"
-              class="mb-0"
+      <b-overlay
+        :show="config.incoming.incomingConnections.length === 0"
+        variant="transparent"
+        blur="5px"
+      >
+        <b-card>
+          <b-row>
+            <b-col
+              md="4"
+              sm="4"
+              class="my-1"
             >
-              <b-input-group size="sm">
-                <b-form-input
-                  id="filterInput"
-                  v-model="config.incoming.filter"
-                  type="search"
-                  placeholder="Type to Search"
-                />
-                <b-input-group-append>
-                  <b-button
-                    :disabled="!config.incoming.filter"
-                    @click="config.incoming.filter = ''"
-                  >
-                    Clear
-                  </b-button>
-                </b-input-group-append>
-              </b-input-group>
-            </b-form-group>
-          </b-col>
-
-          <b-col cols="12">
-            <b-table
-              class="fluxnetwork-incoming-table"
-              striped
-              hover
-              responsive
-              :per-page="config.incoming.perPage"
-              :current-page="config.incoming.currentPage"
-              :items="config.incoming.incomingConnections"
-              :fields="config.incoming.fields"
-              :sort-by.sync="config.incoming.sortBy"
-              :sort-desc.sync="config.incoming.sortDesc"
-              :sort-direction="config.incoming.sortDirection"
-              :filter="config.incoming.filter"
-              :filter-included-fields="config.incoming.filterOn"
-              show-empty
-              empty-text="No Incoming Connections"
-              @filtered="onFilteredIncoming"
-            >
-              <template #cell(disconnect)="row">
-                <b-button
+              <b-form-group class="mb-0">
+                <label class="d-inline-block text-left mr-50">Per page</label>
+                <b-form-select
+                  id="perPageSelect"
+                  v-model="config.incoming.perPage"
                   size="sm"
-                  class="mr-0"
-                  variant="danger"
-                  @click="disconnectIncoming(row)"
-                >
-                  Disconnect
-                </b-button>
-              </template>
-            </b-table>
-          </b-col>
+                  :options="config.incoming.pageOptions"
+                  class="w-50"
+                />
+              </b-form-group>
+            </b-col>
+            <b-col
+              md="8"
+              class="my-1"
+            >
+              <b-form-group
+                label="Filter"
+                label-cols-sm="1"
+                label-align-sm="right"
+                label-for="filterInput"
+                class="mb-0"
+              >
+                <b-input-group size="sm">
+                  <b-form-input
+                    id="filterInput"
+                    v-model="config.incoming.filter"
+                    type="search"
+                    placeholder="Type to Search"
+                  />
+                  <b-input-group-append>
+                    <b-button
+                      :disabled="!config.incoming.filter"
+                      @click="config.incoming.filter = ''"
+                    >
+                      Clear
+                    </b-button>
+                  </b-input-group-append>
+                </b-input-group>
+              </b-form-group>
+            </b-col>
 
-          <b-col cols="12">
-            <b-pagination
-              v-model="config.incoming.currentPage"
-              :total-rows="config.incoming.totalRows"
-              :per-page="config.incoming.perPage"
-              align="center"
-              size="sm"
-              class="my-0"
-            />
-          </b-col>
-        </b-row>
-      </b-card>
+            <b-col cols="12">
+              <b-table
+                class="fluxnetwork-incoming-table"
+                striped
+                hover
+                responsive
+                :per-page="config.incoming.perPage"
+                :current-page="config.incoming.currentPage"
+                :items="config.incoming.incomingConnections"
+                :fields="config.incoming.fields"
+                :sort-by.sync="config.incoming.sortBy"
+                :sort-desc.sync="config.incoming.sortDesc"
+                :sort-direction="config.incoming.sortDirection"
+                :filter="config.incoming.filter"
+                :filter-included-fields="config.incoming.filterOn"
+                show-empty
+                empty-text="No Incoming Connections"
+                @filtered="onFilteredIncoming"
+              >
+                <template #cell(disconnect)="row">
+                  <b-button
+                    size="sm"
+                    class="mr-0"
+                    variant="danger"
+                    @click="disconnectIncoming(row)"
+                  >
+                    Disconnect
+                  </b-button>
+                </template>
+              </b-table>
+            </b-col>
+
+            <b-col cols="12">
+              <b-pagination
+                v-model="config.incoming.currentPage"
+                :total-rows="config.incoming.totalRows"
+                :per-page="config.incoming.perPage"
+                align="center"
+                size="sm"
+                class="my-0"
+              />
+            </b-col>
+          </b-row>
+        </b-card>
+      </b-overlay>
     </b-tab>
   </b-tabs>
 </template>
@@ -209,6 +221,7 @@ import {
   BInputGroupAppend,
   BButton,
   BPagination,
+  BOverlay,
 } from 'bootstrap-vue'
 import FluxService from '@/services/FluxService'
 import ToastificationContent from '@core/components/toastification/ToastificationContent.vue'
@@ -230,6 +243,7 @@ export default {
     BInputGroupAppend,
     BButton,
     BPagination,
+    BOverlay,
     // eslint-disable-next-line vue/no-unused-components
     ToastificationContent,
   },
