373299bf00ac8adec5ab79a27f23959197a09c1a
SyGem Limited <simon@sygem.com>
2021-07-09 16:59:33 +0100

    Overlay spinner for the "Manage Users" screen

--- ZelFront/src/views/fluxadmin/ManageUsers.vue
+++ ZelFront/src/views/fluxadmin/ManageUsers.vue
@@ -1,206 +1,212 @@
 <template>
-  <b-card>
-    <b-row>
-      <b-col
-        md="4"
-        sm="4"
-        class="my-1"
-      >
-        <b-form-group class="mb-0">
-          <label class="d-inline-block text-left mr-50">Per page</label>
-          <b-form-select
-            id="perPageSelect"
-            v-model="perPage"
-            size="sm"
-            :options="pageOptions"
-            class="w-50"
-          />
-        </b-form-group>
-      </b-col>
-      <b-col
-        md="8"
-        class="my-1"
-      >
-        <b-form-group
-          label="Filter"
-          label-cols-sm="1"
-          label-align-sm="right"
-          label-for="filterInput"
-          class="mb-0"
+  <b-overlay
+    :show="usersLoading"
+    variant="transparent"
+    blur="5px"
+  >
+    <b-card>
+      <b-row>
+        <b-col
+          md="4"
+          sm="4"
+          class="my-1"
         >
-          <b-input-group size="sm">
-            <b-form-input
-              id="filterInput"
-              v-model="filter"
-              type="search"
-              placeholder="Type to Search"
-            />
-            <b-input-group-append>
-              <b-button
-                :disabled="!filter"
-                @click="filter = ''"
-              >
-                Clear
-              </b-button>
-            </b-input-group-append>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
-
-      <b-col cols="12">
-        <b-table
-          striped
-          hover
-          responsive
-          small
-          :per-page="perPage"
-          :current-page="currentPage"
-          :items="items"
-          :fields="fields"
-          :sort-by.sync="sortBy"
-          :sort-desc.sync="sortDesc"
-          :sort-direction="sortDirection"
-          :filter="filter"
-          :filter-included-fields="filterOn"
-          show-empty
-          empty-text="No Users"
-          @filtered="onFiltered"
-        >
-          <template #cell(logout)="row">
-            <v-icon
-              v-b-tooltip.hover.top="'Currently logged and used session by you'"
-              name="info-circle"
-              class="mr-1"
-              :class="row.item.loginPhrase === currentLoginPhrase ? '' : 'hidden'"
-            />
-            <b-button
-              :id="`${row.item.loginPhrase}`"
+          <b-form-group class="mb-0">
+            <label class="d-inline-block text-left mr-50">Per page</label>
+            <b-form-select
+              id="perPageSelect"
+              v-model="perPage"
               size="sm"
-              class="mr-0"
-              variant="danger"
-              @click="logoutPopoverShow[row.item.loginPhrase] = true"
-            >
-              Log Out
-            </b-button>
-            <b-popover
-              ref="popover"
-              :target="`${row.item.loginPhrase}`"
-              triggers="click"
-              :show.sync="logoutPopoverShow[row.item.loginPhrase]"
-              placement="auto"
-              container="my-container"
-            >
-              <template v-slot:title>
-                <div class="d-flex justify-content-between align-items-center">
-                  <span>Are You Sure?</span>
+              :options="pageOptions"
+              class="w-50"
+            />
+          </b-form-group>
+        </b-col>
+        <b-col
+          md="8"
+          class="my-1"
+        >
+          <b-form-group
+            label="Filter"
+            label-cols-sm="1"
+            label-align-sm="right"
+            label-for="filterInput"
+            class="mb-0"
+          >
+            <b-input-group size="sm">
+              <b-form-input
+                id="filterInput"
+                v-model="filter"
+                type="search"
+                placeholder="Type to Search"
+              />
+              <b-input-group-append>
+                <b-button
+                  :disabled="!filter"
+                  @click="filter = ''"
+                >
+                  Clear
+                </b-button>
+              </b-input-group-append>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+
+        <b-col cols="12">
+          <b-table
+            striped
+            hover
+            responsive
+            small
+            :per-page="perPage"
+            :current-page="currentPage"
+            :items="items"
+            :fields="fields"
+            :sort-by.sync="sortBy"
+            :sort-desc.sync="sortDesc"
+            :sort-direction="sortDirection"
+            :filter="filter"
+            :filter-included-fields="filterOn"
+            show-empty
+            empty-text="No Users"
+            @filtered="onFiltered"
+          >
+            <template #cell(logout)="row">
+              <v-icon
+                v-b-tooltip.hover.top="'Currently logged and used session by you'"
+                name="info-circle"
+                class="mr-1"
+                :class="row.item.loginPhrase === currentLoginPhrase ? '' : 'hidden'"
+              />
+              <b-button
+                :id="`${row.item.loginPhrase}`"
+                size="sm"
+                class="mr-0"
+                variant="danger"
+                @click="logoutPopoverShow[row.item.loginPhrase] = true"
+              >
+                Log Out
+              </b-button>
+              <b-popover
+                ref="popover"
+                :target="`${row.item.loginPhrase}`"
+                triggers="click"
+                :show.sync="logoutPopoverShow[row.item.loginPhrase]"
+                placement="auto"
+                container="my-container"
+              >
+                <template v-slot:title>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <span>Are You Sure?</span>
+                    <b-button
+                      v-ripple.400="'rgba(255, 255, 255, 0.15)'"
+                      class="close"
+                      variant="transparent"
+                      aria-label="Close"
+                      @click="onLogoutClose(row.item)"
+                    >
+                      <span
+                        class="d-inline-block text-white"
+                        aria-hidden="true"
+                      >&times;</span>
+                    </b-button>
+                  </div>
+                </template>
+
+                <div>
                   <b-button
                     v-ripple.400="'rgba(255, 255, 255, 0.15)'"
-                    class="close"
-                    variant="transparent"
-                    aria-label="Close"
+                    size="sm"
+                    variant="danger"
+                    class="mr-1"
                     @click="onLogoutClose(row.item)"
                   >
-                    <span
-                      class="d-inline-block text-white"
-                      aria-hidden="true"
-                    >&times;</span>
+                    Cancel
+                  </b-button>
+                  <b-button
+                    v-ripple.400="'rgba(255, 255, 255, 0.15)'"
+                    size="sm"
+                    variant="primary"
+                    @click="onLogoutOK(row.item)"
+                  >
+                    Log Out!
                   </b-button>
                 </div>
-              </template>
+              </b-popover>
+            </template>
+          </b-table>
+        </b-col>
 
-              <div>
-                <b-button
-                  v-ripple.400="'rgba(255, 255, 255, 0.15)'"
-                  size="sm"
-                  variant="danger"
-                  class="mr-1"
-                  @click="onLogoutClose(row.item)"
-                >
-                  Cancel
-                </b-button>
-                <b-button
-                  v-ripple.400="'rgba(255, 255, 255, 0.15)'"
-                  size="sm"
-                  variant="primary"
-                  @click="onLogoutOK(row.item)"
-                >
-                  Log Out!
-                </b-button>
-              </div>
-            </b-popover>
-          </template>
-        </b-table>
-      </b-col>
-
-      <b-col cols="12">
-        <b-pagination
-          v-model="currentPage"
-          :total-rows="totalRows"
-          :per-page="perPage"
-          align="center"
+        <b-col cols="12">
+          <b-pagination
+            v-model="currentPage"
+            :total-rows="totalRows"
+            :per-page="perPage"
+            align="center"
+            size="sm"
+            class="my-0"
+          />
+        </b-col>
+      </b-row>
+      <div class="text-center">
+        <b-button
+          id="logout-all"
           size="sm"
-          class="my-0"
-        />
-      </b-col>
-    </b-row>
-    <div class="text-center">
-      <b-button
-        id="logout-all"
-        size="sm"
-        class="mt-2"
-        variant="danger"
-        @click="logoutAllPopoverShow = true"
-      >
-        Log Out all Users
-      </b-button>
-      <b-popover
-        ref="popover"
-        target="logout-all"
-        triggers="click"
-        :show.sync="logoutAllPopoverShow"
-        placement="auto"
-        container="my-container"
-      >
-        <template v-slot:title>
-          <div class="d-flex justify-content-between align-items-center">
-            <span>Are You Sure?</span>
+          class="mt-2"
+          variant="danger"
+          @click="logoutAllPopoverShow = true"
+        >
+          Log Out all Users
+        </b-button>
+        <b-popover
+          ref="popover"
+          target="logout-all"
+          triggers="click"
+          :show.sync="logoutAllPopoverShow"
+          placement="auto"
+          container="my-container"
+        >
+          <template v-slot:title>
+            <div class="d-flex justify-content-between align-items-center">
+              <span>Are You Sure?</span>
+              <b-button
+                v-ripple.400="'rgba(255, 255, 255, 0.15)'"
+                class="close"
+                variant="transparent"
+                aria-label="Close"
+                @click="onLogoutAllClose()"
+              >
+                <span
+                  class="d-inline-block text-white"
+                  aria-hidden="true"
+                >&times;</span>
+              </b-button>
+            </div>
+          </template>
+
+          <div>
             <b-button
               v-ripple.400="'rgba(255, 255, 255, 0.15)'"
-              class="close"
-              variant="transparent"
-              aria-label="Close"
+              size="sm"
+              variant="danger"
+              class="mr-1"
               @click="onLogoutAllClose()"
             >
-              <span
-                class="d-inline-block text-white"
-                aria-hidden="true"
-              >&times;</span>
+              Cancel
+            </b-button>
+            <b-button
+              v-ripple.400="'rgba(255, 255, 255, 0.15)'"
+              size="sm"
+              variant="primary"
+              @click="onLogoutAllOK()"
+            >
+              Log Out All!
             </b-button>
           </div>
-        </template>
-
-        <div>
-          <b-button
-            v-ripple.400="'rgba(255, 255, 255, 0.15)'"
-            size="sm"
-            variant="danger"
-            class="mr-1"
-            @click="onLogoutAllClose()"
-          >
-            Cancel
-          </b-button>
-          <b-button
-            v-ripple.400="'rgba(255, 255, 255, 0.15)'"
-            size="sm"
-            variant="primary"
-            @click="onLogoutAllOK()"
-          >
-            Log Out All!
-          </b-button>
-        </div>
-      </b-popover>
-    </div>
-  </b-card>
+        </b-popover>
+      </div>
+    </b-card>
+  </b-overlay>
 </template>
 
 <script>
@@ -218,6 +224,7 @@ import {
   BButton,
   VBTooltip,
   BPopover,
+  BOverlay,
 } from 'bootstrap-vue'
 import IDService from '@/services/IDService'
 import ToastificationContent from '@core/components/toastification/ToastificationContent.vue'
@@ -239,6 +246,7 @@ export default {
     BInputGroupAppend,
     BButton,
     BPopover,
+    BOverlay,
     // eslint-disable-next-line vue/no-unused-components
     ToastificationContent,
   },
@@ -265,6 +273,7 @@ export default {
       currentPage: 1,
       logoutPopoverShow: {},
       logoutAllPopoverShow: false,
+      usersLoading: true,
     }
   },
   computed: {
@@ -285,6 +294,7 @@ export default {
   },
   methods: {
     async loggedUsers() {
+      this.usersLoading = true
       const zelidauth = localStorage.getItem('zelidauth')
       IDService.loggedUsers(zelidauth)
         .then(response => {
@@ -302,6 +312,7 @@ export default {
             this.totalRows = this.items.length
             this.currentPage = 1
           }
+          this.usersLoading = false
         })
         .catch(e => {
           console.log(e)
@@ -313,6 +324,7 @@ export default {
               variant: 'danger',
             },
           })
+          this.usersLoading = false
         })
     },
     onFiltered(filteredItems) {
