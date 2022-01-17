1e4a4c3b1933444971e854b2ea5809e50aee3faf
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2022-01-17 11:25:53 +0700

    simplify stopAllNonFluxRunningApps version

--- ZelBack/src/services/appsService.js
+++ ZelBack/src/services/appsService.js
@@ -6208,21 +6208,25 @@ async function reconstructAppMessagesHashCollectionAPI(req, res) {
 }
 
 async function stopAllNonFluxRunningApps() {
-  log.info('Flux Stop All Non Flux Running Apps Started');
   try {
+    log.info('Running non Flux apps check...');
     let apps = await dockerService.dockerListContainers(false);
+    apps = apps.filter((app) => (app.Names[0].substr(1, 3) !== 'zel' && app.Names[0].substr(1, 4) !== 'flux'));
     if (apps.length > 0) {
-      apps = apps.filter((app) => (app.Names[0].substr(1, 3) !== 'zel' && app.Names[0].substr(1, 4) !== 'flux'));
-    }
-    log.info(`Found ${apps.length} apps to be stopped.`);
-    for (let i = 0; i < apps.length; i += 1) {
-      const app = apps[i];
-      log.warning(`Stopping non flux application: ${app.Names[0]}`);
-      // eslint-disable-next-line no-await-in-loop
-      await dockerService.appDockerStop(app.Id);
-    }
-    if (apps.length > 0) {
-      log.info('All apps stopped.');
+      log.info(`Found ${apps.length} apps to be stopped...`);
+      // eslint-disable-next-line no-restricted-syntax
+      for (const app of apps) {
+        try {
+          log.info(`Stopping non Flux app ${app.Names[0]}`);
+          // eslint-disable-next-line no-await-in-loop
+          await dockerService.appDockerStop(app.Id); // continue if failed to stop one app
+          log.info(`Non Flux app ${app.Names[0]} stopped.`);
+        } catch (error) {
+          log.error(`Failed to stop non Flux app ${app.Names[0]}.`);
+        }
+      }
+    } else {
+      log.info('Only Flux apps are running.');
     }
     setTimeout(() => {
       stopAllNonFluxRunningApps();
@@ -6231,7 +6235,7 @@ async function stopAllNonFluxRunningApps() {
     log.error(error);
     setTimeout(() => {
       stopAllNonFluxRunningApps();
-    }, 30 * 60 * 1000); // In case of a error execute after 30m
+    }, 30 * 60 * 1000); // In case of an error execute after 30m
   }
 }
 
