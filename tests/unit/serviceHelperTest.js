c2dedb0ac110c0d55167b9435f00242c66e50de9
syed ahmad <szahmad124@gmail.com.com>
2022-02-03 12:25:09 -0500

    update serviceHelperTest.js with DB test

--- tests/unit/serviceHelperTest.js
+++ tests/unit/serviceHelperTest.js
@@ -3,11 +3,19 @@ const serviceHelper = require("../../ZelBack/src/services/serviceHelper");
 const config = require('../../ZelBack/config/default');
 const chai = require('chai')
 const assert = require('assert')
+const log = require('../../ZelBack/src/lib/log');
+const { doesNotMatch } = require("assert");
+const express = require('express');
 const expect = chai.expect
 const addressTransactionIndexCollection = config.database.daemon.collections.addressTransactionIndex;
 const localAppsInformation = config.database.appslocal.collections.appsInformation;
-
+const globalAppsInformation = config.database.appsglobal.collections.appsInformation;
 describe('serviceHelper Functions', () => {
+    let mochaAsync = (fn) => {
+        return (done) => {
+            fn.call().then(done, (err) => { done(err) });
+        };
+    };
     describe('ensureBoolean', () => {
         it('should ensure that parameter is boolean', () => {
             expect(serviceHelper.ensureBoolean('false')).to.equal(false)
@@ -97,18 +105,232 @@ describe('serviceHelper Functions', () => {
         })
     })
 
-    describe('initiateDB', () => {
-        it('should ensure that DB is initiated', () => {
-            serviceHelper.initiateDB().then((res) => {
-                expect(res).to.equal(true)
-            })
-        })
+    // describe('initiateDB', () => {
+    //     it('should ensure that DB is initiated', () => {
+    //         serviceHelper.initiateDB().then((res) => {
+    //             expect(res).to.equal(true)
+    //         })
+    //     })
+    // })
+
+    // describe('databaseConnection', () => {
+    //     it('should ensure that it gets DB Connection object', () => {
+    //         expect(serviceHelper.databaseConnection()).to.equal(null)
+    //     })
+    // })
+
+    // describe('distinctDatabase', () => {
+    //     it('should ensure that it gets DB Connection object', async ()=> {
+    //         serviceHelper.initiateDB().then(() => {
+    //            // console.log("hi hamid")
+    //           // assert.equal(true,true)
+    //         })
+    //     })
+    // })
+
+
+
+    // describe('distinctDatabase', () => {
+    //     let mochaAsync = (fn) => {
+    //         return (done) => {
+    //           fn.call().then(done, (err)=>{done(err)});
+    //         };
+    //       };
+    //     beforeEach(mochaAsync(async () => {
+    //         await serviceHelper.initiateDB();
+    //     }));
+       
+    //       it("Sample async/await mocha test using wrapper", mochaAsync(async () => {
+    //         await serviceHelper.initiateDB();
+    //         var MockExpressRequest = require('mock-express-request');
+    //         var MockExpressResponse = require('mock-express-response');
+    //         var res = new MockExpressResponse();
+    //         const dbopen = serviceHelper.databaseConnection();
+    //         const database = dbopen.db(config.database.daemon.database);
+    //         const variable = 'address';
+    //         const results =  serviceHelper.distinctDatabase(database, addressTransactionIndexCollection, variable);
+    //         const resMessage = serviceHelper.createDataMessage(results);
+    //         console.log("hello",results)
+    //         res.json(resMessage);
+    //     }));
+    // })
+//////////////////////////////
+    
+    describe('connectMongoDb', () => {
+        before(mochaAsync(async () => {
+            await serviceHelper.initiateDB();
+        }))
+        it("should ensure that MongoDB is connected", mochaAsync(async () => {
+            openDBConnection = await serviceHelper.connectMongoDb();
+            expect(typeof (openDBConnection)).to.be.equal('object')
+        }));
+    })
+    describe('distinctDatabase', () => {
+        before(mochaAsync(async () => {
+            await serviceHelper.initiateDB();
+        }))
+        it("should ensure that distinctDatabase returns success message", mochaAsync(async () => {
+            try {
+                const dbopen = serviceHelper.databaseConnection();
+                const database = dbopen.db(config.database.daemon.database);
+                const variable = 'address';
+                var MockExpressResponse = require('mock-express-response');
+                var res = new MockExpressResponse();
+                const results = await serviceHelper.distinctDatabase(database, addressTransactionIndexCollection, variable);
+                const resMessage = serviceHelper.createDataMessage(results);
+               expect(resMessage.status).to.equal('success')
+              } catch (error) {
+                log.error(error);
+                const errMessage = serviceHelper.createErrorMessage(error.message, error.name, error.code);
+                res.json(errMessage);
+              }
+        }));
     })
 
-    describe('databaseConnection', () => {
-        it('should ensure that it gets DB Connection object', () => {
-            expect(serviceHelper.databaseConnection()).to.equal(null)
-        })
+    describe('findInDatabase', () => {
+        before(mochaAsync(async () => {
+            await serviceHelper.initiateDB();
+        }))
+        it("should ensure that findInDatabase works fine", mochaAsync(async () => {
+            try {
+                const dbopen = serviceHelper.databaseConnection();
+                const appsDatabase = dbopen.db(config.database.appslocal.database);
+                const appsQuery = {};
+                var MockExpressResponse = require('mock-express-response');
+                var res = new MockExpressResponse();
+                const appsProjection = { projection: { _id: 0 } };
+                const messages = await serviceHelper.findInDatabase(appsDatabase, localAppsInformation, appsQuery, appsProjection);
+                console.log("msg",messages)
+              } catch (error) {
+                log.error(error);
+                const errMessage = serviceHelper.createErrorMessage(error.message, error.name, error.code);
+                res.json(errMessage);
+              }
+        }));
     })
 
+
+    describe('findOneAndUpdateInDatabase', () => {
+        before(mochaAsync(async () => {
+            await serviceHelper.initiateDB();
+        }))
+        it("should ensure that findOneAndUpdateInDatabase works fine", mochaAsync(async () => {
+            try {
+                var MockExpressResponse = require('mock-express-response');
+                var res = new MockExpressResponse();
+                const dbopen = serviceHelper.databaseConnection();
+                const appsDatabase = dbopen.db(config.database.appslocal.database);
+                const appsQuery = {};
+                const messages = await serviceHelper.findOneAndUpdateInDatabase(appsDatabase, collection, appsQuery, update, options)
+              } catch (error) {
+                log.error(error);
+                const errMessage = serviceHelper.createErrorMessage(error.message, error.name, error.code);
+                res.json(errMessage);
+              }
+        }));
+    })
+
+    describe('insertOneToDatabase', () => {
+        before(mochaAsync(async () => {
+            await serviceHelper.initiateDB();
+        }))
+        it("should ensure that insertOneToDatabase works fine", mochaAsync(async () => {
+            try {
+                var MockExpressResponse = require('mock-express-response');
+                var res = new MockExpressResponse();
+                const dbopen = serviceHelper.databaseConnection();
+                const databaseFluxShare = dbopen.db(config.database.fluxshare.database);
+                const sharedCollection = config.database.fluxshare.collections.shared;
+                const string = file + new Date().getTime().toString() + Math.floor((Math.random() * 999999999999999)).toString();
+              
+                const fileDetail = {
+                    name: file,
+                    token: crypto.createHash('sha256').update(string).digest('hex'),
+                  };
+                const messages = await serviceHelper.insertOneToDatabase(databaseFluxShare, sharedCollection, fileDetail)
+              } catch (error) {
+                log.error(error);
+                const errMessage = serviceHelper.createErrorMessage(error.message, error.name, error.code);
+                res.json(errMessage);
+              }
+        }));
+    })
+
+    describe('updateInDatabase', () => {
+        before(mochaAsync(async () => {
+            await serviceHelper.initiateDB();
+        }))
+        it("should ensure that updateInDatabase works fine", mochaAsync(async () => {
+            try {
+                const dbopen = serviceHelper.databaseConnection();
+                const appsDatabase = dbopen.db(config.database.appslocal.database);
+                const appsQuery = {};
+                const appsProjection = { projection: { _id: 0 } };
+                const messages = await serviceHelper.findInDatabase(appsDatabase, localAppsInformation, appsQuery, appsProjection);
+              } catch (error) {
+                log.error(error);
+                const errMessage = serviceHelper.createErrorMessage(error.message, error.name, error.code);
+                res.json(errMessage);
+              }
+        }));
+    })
+
+    describe('updateOneInDatabase', () => {
+        before(mochaAsync(async () => {
+            await serviceHelper.initiateDB();
+        }))
+        it("should ensure that updateOneInDatabase works fine", mochaAsync(async () => {
+            try {
+                const dbopen = serviceHelper.databaseConnection();
+                const appsDatabase = dbopen.db(config.database.appslocal.database);
+                const appsQuery = {};
+                const appsProjection = { projection: { _id: 0 } };
+                const messages = await serviceHelper.findInDatabase(appsDatabase, localAppsInformation, appsQuery, appsProjection);
+              } catch (error) {
+                log.error(error);
+                const errMessage = serviceHelper.createErrorMessage(error.message, error.name, error.code);
+                res.json(errMessage);
+              }
+        }));
+    })
+
+    describe('findOneAndDeleteInDatabase', () => {
+        before(mochaAsync(async () => {
+            await serviceHelper.initiateDB();
+        }))
+        it("should ensure that findOneAndDeleteInDatabase works fine", mochaAsync(async () => {
+            try {
+                const dbopen = serviceHelper.databaseConnection();
+                var MockExpressResponse = require('mock-express-response');
+                var res = new MockExpressResponse();
+                const databaseApps = dbopen.db(config.database.appsglobal.database);
+                const projectionApps = { projection: { _id: 0, name: 1, hash: 1 } };
+                const query={}
+                const messages = await serviceHelper.findOneAndDeleteInDatabase(databaseApps, collection, query, projection)
+              } catch (error) {
+                log.error(error);
+                const errMessage = serviceHelper.createErrorMessage(error.message, error.name, error.code);
+                res.json(errMessage);
+              }
+        }));
+    })
+
+    describe('removeDocumentsFromCollection', () => {
+        before(mochaAsync(async () => {
+            await serviceHelper.initiateDB();
+        }))
+        it("should ensure that removeDocumentsFromCollection works fine", mochaAsync(async () => {
+            try {
+                const dbopen = serviceHelper.databaseConnection();
+                const database = dbopen.db(config.database.local.database);
+                const collection = config.database.local.collections.loggedUsers;
+                const query={}
+                const messages = await serviceHelper.removeDocumentsFromCollection(database, collection, query)
+              } catch (error) {
+                log.error(error);
+                const errMessage = serviceHelper.createErrorMessage(error.message, error.name, error.code);
+                res.json(errMessage);
+              }
+        }));
+    })
 })
