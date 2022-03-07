2653e2f7af100741cab0169a05b4d01ab52bd8a8
Tadeas Kmenta <kmentat@gmail.com>
2022-03-07 11:42:51 +0700

    revert adjust confirmNodeTierHardware

--- ZelBack/src/services/idService.js
+++ ZelBack/src/services/idService.js
@@ -21,9 +21,13 @@ const goodchars = /^[1-9a-km-zA-HJ-NP-Z]+$/;
  */
 async function confirmNodeTierHardware() {
   try {
-    const tier = await generalService.nodeTier();
+    const tier = await generalService.nodeTier().catch((error) => {
+      log.error(error);
+    });
 
-    const collateral = await generalService.nodeCollateral();
+    const collateral = await generalService.nodeCollateral().catch((error) => {
+      log.error(error);
+    });
     const nodeRam = os.totalmem() / 1024 / 1024 / 1024;
     const nodeCpuThreads = os.cpus().length;
     log.info(`Node Tier: ${tier}`);
@@ -72,8 +76,6 @@ async function confirmNodeTierHardware() {
       if (nodeCpuThreads < 4) {
         throw new Error(`Node Cpu Threads (${nodeCpuThreads}) below new Cumulus requirements`);
       }
-    } else {
-      throw new Error('Unkown tier');
     }
     return true;
   } catch (error) {
