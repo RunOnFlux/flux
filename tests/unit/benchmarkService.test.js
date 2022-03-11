9a0dc777278bdf1c01e80b877cc567be57b2edd6
JacekAdamczyk <jacek.adamczyk@gmail.com>
2022-03-11 17:29:02 +0100

    signFluxTransaction and restartNodeBenchmarks tests

--- tests/unit/benchmarkService.test.js
+++ tests/unit/benchmarkService.test.js
@@ -3,6 +3,9 @@ const sinon = require('sinon');
 const benchmarkrpc = require('daemonrpc');
 const proxyquire = require('proxyquire');
 
+const { expect } = chai;
+const verificationHelper = require('../../ZelBack/src/services/verificationHelper');
+
 const adminConfig = {
   initial: {
     ipaddress: '83.51.212.243',
@@ -12,8 +15,6 @@ const adminConfig = {
 };
 const benchmarkService = proxyquire('../../ZelBack/src/services/benchmarkService',
   { '../../../config/userconfig': adminConfig });
-
-const { expect } = chai;
 
 describe('benchmarkService tests', () => {
   describe('executeCall tests', () => {
@@ -79,7 +80,7 @@ describe('benchmarkService tests', () => {
       sinon.assert.calledOnce(benchmarkStub);
     });
 
-    it('should handle properly if response object is passed', async () => {
+    it('should call getstatus properly if response object is passed', async () => {
       const generateResponse = () => {
         const res = { test: 'testing' };
         res.status = sinon.stub().returns(res);
@@ -91,7 +92,179 @@ describe('benchmarkService tests', () => {
 
       await benchmarkService.getStatus(undefined, mockResponse);
 
+      sinon.assert.calledOnce(benchmarkStub);
       sinon.assert.calledOnceWithExactly(mockResponse.json, expectedSuccessMessage);
+    });
+  });
+
+  describe('restartNodeBenchmarks tests', () => {
+    let benchmarkStub;
+    let verificationHelperStub;
+
+    beforeEach(async () => {
+      benchmarkStub = sinon.stub(benchmarkrpc.Client.prototype, 'restartnodebenchmarks').returns(Promise.resolve('called'));
+    });
+
+    afterEach(() => {
+      benchmarkStub.restore();
+      verificationHelperStub.restore();
+    });
+
+    // No need to test specific privilege - function has been tested in verification helper
+    it('should return a sucessful response if called without parameters by an authorized party', async () => {
+      verificationHelperStub = sinon.stub(verificationHelper, 'verifyPrivilege').returns(true);
+      const req = {
+        headers: {
+          zelidauth: {
+            zelid: '1CbErtneaX2QVyUfwU7JGB7VzvPgrgc3uC',
+            signature: 'IH9d68fk/dYQtuMlNN7ioc52MJ6ryRT0IYss6h/KCwVWGcbVNFoI8Jh6hIklRq+w2itV/6vs/xzCWp4TUdSWDBc=',
+          },
+        },
+      };
+
+      const restartNodeBenchmarksResult = await benchmarkService.restartNodeBenchmarks(req);
+
+      expect(restartNodeBenchmarksResult).to.be.an('object');
+      expect(restartNodeBenchmarksResult.status).to.equal('success');
+      expect(restartNodeBenchmarksResult.data).to.equal('called');
+      sinon.assert.calledOnceWithExactly(verificationHelperStub, 'adminandfluxteam', req);
+    });
+
+    // No need to test specific privilege - function has been tested in verification helper
+    it('should return a sucessful response if called with parameters by an authorized party', async () => {
+      verificationHelperStub = sinon.stub(verificationHelper, 'verifyPrivilege').returns(true);
+      const req = {
+        headers: {
+          zelidauth: {
+            zelid: '1CbErtneaX2QVyUfwU7JGB7VzvPgrgc3uC',
+            signature: 'IH9d68fk/dYQtuMlNN7ioc52MJ6ryRT0IYss6h/KCwVWGcbVNFoI8Jh6hIklRq+w2itV/6vs/xzCWp4TUdSWDBc=',
+          },
+        },
+      };
+      const generateResponse = () => {
+        const res = { test: 'testing' };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns(res);
+        return res;
+      };
+      const mockResponse = generateResponse();
+      const expectedSuccessMessage = { status: 'success', data: 'called' };
+
+      await benchmarkService.restartNodeBenchmarks(req, mockResponse);
+
+      sinon.assert.calledOnce(benchmarkStub);
+      sinon.assert.calledOnceWithExactly(mockResponse.json, expectedSuccessMessage);
+      sinon.assert.calledOnceWithExactly(verificationHelperStub, 'adminandfluxteam', req);
+    });
+
+    // No need to test specific privilege - function has been tested in verification helper
+    it('should return an access denied response if called without parameters by an unauthorized party', async () => {
+      verificationHelperStub = sinon.stub(verificationHelper, 'verifyPrivilege').returns(false);
+      const req = {
+        headers: {
+          zelidauth: {
+            zelid: '1CbErtneaX2QVyUfwU7JGB7VzvPgrgc3uV',
+            signature: 'IH9d68fk/dYQtuMlNN7ioc52MJ6ryRT0IYss6h/KCwVWGcbVNFoI8Jh6hIklRq+w2itV/6vs/xzCWp4TUdSWDBx=',
+          },
+        },
+      };
+      const restartNodeBenchmarksResult = await benchmarkService.restartNodeBenchmarks(req);
+
+      expect(restartNodeBenchmarksResult).to.be.an('object');
+      expect(restartNodeBenchmarksResult.status).to.equal('error');
+      sinon.assert.calledOnceWithExactly(verificationHelperStub, 'adminandfluxteam', req);
+      sinon.assert.notCalled(benchmarkStub);
+    });
+  });
+
+  describe('signFluxTransaction tests', () => {
+    let benchmarkStub;
+    let verificationHelperStub;
+
+    beforeEach(async () => {
+      benchmarkStub = sinon.stub(benchmarkrpc.Client.prototype, 'signzelnodetransaction').returns(Promise.resolve('called'));
+    });
+
+    afterEach(() => {
+      benchmarkStub.restore();
+      verificationHelperStub.restore();
+    });
+
+    // No need to test specific privilege - function has been tested in verification helper
+    it('should return a sucessful response if called without parameters by an authorized party', async () => {
+      verificationHelperStub = sinon.stub(verificationHelper, 'verifyPrivilege').returns(true);
+      const req = {
+        headers: {
+          zelidauth: {
+            zelid: '1CbErtneaX2QVyUfwU7JGB7VzvPgrgc3uC',
+            signature: 'IH9d68fk/dYQtuMlNN7ioc52MJ6ryRT0IYss6h/KCwVWGcbVNFoI8Jh6hIklRq+w2itV/6vs/xzCWp4TUdSWDBc=',
+          },
+        },
+        params: {
+          hexstring: '0x0123ABCDF',
+        },
+      };
+
+      const signFluxTransactionResult = await benchmarkService.signFluxTransaction(req);
+
+      expect(signFluxTransactionResult).to.be.an('object');
+      expect(signFluxTransactionResult.status).to.equal('success');
+      expect(signFluxTransactionResult.data).to.equal('called');
+      sinon.assert.calledOnceWithExactly(verificationHelperStub, 'admin', req);
+      sinon.assert.calledOnceWithExactly(benchmarkStub, req.params.hexstring);
+    });
+
+    // No need to test specific privilege - function has been tested in verification helper
+    it('should return a sucessful response if called with parameters by an authorized party', async () => {
+      verificationHelperStub = sinon.stub(verificationHelper, 'verifyPrivilege').returns(true);
+      const req = {
+        headers: {
+          zelidauth: {
+            zelid: '1CbErtneaX2QVyUfwU7JGB7VzvPgrgc3uZ',
+            signature: 'IH9d68fk/dYQtuMlNx7ioc52MJ6ryRT0IYss6h/KCwVWGcbVNFoI8Jh6hIklRq+w2itV/6vs/xzCWp4TUdSWDBc=',
+          },
+        },
+        params: {
+          hexstring: '0x0123ABCDF',
+        },
+      };
+      const generateResponse = () => {
+        const res = { test: 'testing' };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns(res);
+        return res;
+      };
+      const mockResponse = generateResponse();
+      const expectedSuccessMessage = { status: 'success', data: 'called' };
+
+      await benchmarkService.signFluxTransaction(req, mockResponse);
+
+      sinon.assert.calledOnce(benchmarkStub);
+      sinon.assert.calledOnceWithExactly(mockResponse.json, expectedSuccessMessage);
+      sinon.assert.calledOnceWithExactly(verificationHelperStub, 'admin', req);
+      sinon.assert.calledOnceWithExactly(benchmarkStub, req.params.hexstring);
+    });
+
+    // No need to test specific privilege - function has been tested in verification helper
+    it('should return an access denied response if called without parameters by an unauthorized party', async () => {
+      verificationHelperStub = sinon.stub(verificationHelper, 'verifyPrivilege').returns(false);
+      const req = {
+        headers: {
+          zelidauth: {
+            zelid: '1CbErtneaX2QVyUfwU7JGB7VzvPgrgc3uC',
+            signature: 'IH9d68fk/dYQtuMlNN7ioc52MJ6ryRT0IYss6h/KCwVWGcbVNFoI8Jh6hIklRq+w2itV/6vs/xzCWp4TUdSWDBc=',
+          },
+        },
+        params: {
+          hexstring: '0x0123ABCDF',
+        },
+      };
+      const signFluxTransactionResult = await benchmarkService.signFluxTransaction(req);
+
+      expect(signFluxTransactionResult).to.be.an('object');
+      expect(signFluxTransactionResult.status).to.equal('error');
+      sinon.assert.calledOnceWithExactly(verificationHelperStub, 'admin', req);
+      sinon.assert.notCalled(benchmarkStub);
     });
   });
 });
