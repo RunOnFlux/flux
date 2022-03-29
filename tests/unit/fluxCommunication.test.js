4851a034eda2bd05f7885a87b6a2a629ca0d1b1b
JacekAdamczyk <jacek.adamczyk@gmail.com>
2022-03-29 17:09:32 +0200

    more flux communication tests

--- tests/unit/fluxCommunication.test.js
+++ tests/unit/fluxCommunication.test.js
@@ -2,12 +2,14 @@
 const sinon = require('sinon');
 const WebSocket = require('ws');
 
+const { expect } = require('chai');
 const fluxCommunication = require('../../ZelBack/src/services/fluxCommunication');
 const fluxCommunicationMessagesSender = require('../../ZelBack/src/services/fluxCommunicationMessagesSender');
 const dbHelper = require('../../ZelBack/src/services/dbHelper');
 const verificationHelper = require('../../ZelBack/src/services/verificationHelper');
 const generalService = require('../../ZelBack/src/services/generalService');
 const { outgoingConnections } = require('../../ZelBack/src/services/utils/outgoingConnections');
+const { outgoingPeers } = require('../../ZelBack/src/services/utils/outgoingPeers');
 const { incomingConnections } = require('../../ZelBack/src/services/utils/incomingConnections');
 
 const connectWs = (address) => new Promise(((resolve, reject) => {
@@ -300,5 +302,285 @@ describe('fluxCommunication tests', () => {
       sinon.assert.notCalled(sendToAllPeersSpy);
       sinon.assert.notCalled(sendToAllIncomingConnectionsSpy);
     }).timeout(5000);
+  });
+
+  describe('connectedPeeers tests', () => {
+    const generateResponse = () => {
+      const res = { test: 'testing' };
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.fake((param) => param);
+      return res;
+    };
+
+    afterEach(() => {
+      outgoingConnections.length = 0;
+      sinon.restore();
+    });
+
+    it('should return connected peers\'s ips', async () => {
+      const wsuri = 'wss://api.runonflux.io/ws/flux/';
+      const wsuri2 = 'wss://api.runonflux.io/ws/flux2/';
+      const wsOutgoing1 = await connectWs(wsuri);
+      wsOutgoing1._socket = { remoteAddress: '127.8.8.1' };
+      outgoingConnections.push(wsOutgoing1);
+      const wsOutgoing2 = await connectWs(wsuri2);
+      wsOutgoing2._socket = { remoteAddress: '127.8.8.2' };
+      outgoingConnections.push(wsOutgoing2);
+      const expectedResult = { status: 'success', data: ['127.8.8.1', '127.8.8.2'] };
+
+      const res = generateResponse();
+
+      await fluxCommunication.connectedPeers(undefined, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, expectedResult);
+    });
+
+    it('should empty list if no peers are connected', async () => {
+      const res = generateResponse();
+      const expectedResult = { status: 'success', data: [] };
+
+      await fluxCommunication.connectedPeers(undefined, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, expectedResult);
+    });
+  });
+
+  describe('connectedPeeersInfo tests', () => {
+    const generateResponse = () => {
+      const res = { test: 'testing' };
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.fake((param) => param);
+      return res;
+    };
+
+    afterEach(() => {
+      outgoingPeers.length = 0;
+      sinon.restore();
+    });
+
+    it('should return connected connected websockets', async () => {
+      const peer1 = {
+        ip: '127.0.3.1', // can represent just one ip address, multiport
+        lastPingTime: null,
+        latency: null,
+      };
+      const peer2 = {
+        ip: '192.168.0.0', // can represent just one ip address, multiport
+        lastPingTime: new Date().getTime(),
+        latency: 50,
+      };
+      outgoingPeers.push(peer1);
+      outgoingPeers.push(peer2);
+      const expectedResult = {
+        status: 'success',
+        data: [
+          peer1,
+          peer2,
+        ],
+      };
+
+      const res = generateResponse();
+
+      await fluxCommunication.connectedPeersInfo(undefined, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, expectedResult);
+    });
+
+    it('should empty list if no peers are connected', async () => {
+      const res = generateResponse();
+      const expectedResult = { status: 'success', data: [] };
+
+      await fluxCommunication.connectedPeersInfo(undefined, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, expectedResult);
+    });
+  });
+
+  describe.only('removePeer tests', () => {
+    let verificationHelperStub;
+
+    beforeEach(() => {
+      outgoingPeers.length = 0;
+
+      const peer1 = {
+        ip: '127.0.3.1',
+        lastPingTime: null,
+        latency: null,
+      };
+      const peer2 = {
+        ip: '192.168.0.0',
+        lastPingTime: new Date().getTime(),
+        latency: 50,
+      };
+      outgoingPeers.push(peer1);
+      outgoingPeers.push(peer2);
+    });
+
+    afterEach(() => {
+      outgoingConnections.length = 0;
+      outgoingPeers.length = 0;
+      sinon.restore();
+    });
+
+    it('should close the connection with ip given in params if it exists', async () => {
+      const wsuri = 'wss://api.runonflux.io/ws/flux/';
+      const wsOutgoing1 = await connectWs(wsuri);
+      wsOutgoing1._socket = { remoteAddress: '127.0.3.1' };
+      wsOutgoing1.close = () => true;
+      outgoingConnections.push(wsOutgoing1);
+      verificationHelperStub = sinon.stub(verificationHelper, 'verifyPrivilege').returns(true);
+      const expectedResult = {
+        status: 'success',
+        data: {
+          code: undefined,
+          name: undefined,
+          message: 'Outgoing connection to 127.0.3.1 closed',
+        },
+      };
+      const req = {
+        params: {
+          ip: '127.0.3.1',
+        },
+      };
+      const generateResponse = () => {
+        const res = { test: 'testing' };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.fake((param) => param);
+        return res;
+      };
+      const res = generateResponse();
+
+      const result = await fluxCommunication.removePeer(req, res);
+
+      expect(result).to.eql(expectedResult);
+      sinon.assert.calledOnceWithExactly(verificationHelperStub, 'adminandfluxteam', req);
+    });
+
+    it('should close the connection with ip given in query if it exists', async () => {
+      const wsuri = 'wss://api.runonflux.io/ws/flux/';
+      const wsOutgoing1 = await connectWs(wsuri);
+      wsOutgoing1._socket = { remoteAddress: '127.0.3.1' };
+      wsOutgoing1.close = () => true;
+      outgoingConnections.push(wsOutgoing1);
+      verificationHelperStub = sinon.stub(verificationHelper, 'verifyPrivilege').returns(true);
+      const expectedResult = {
+        status: 'success',
+        data: {
+          code: undefined,
+          name: undefined,
+          message: 'Outgoing connection to 127.0.3.1 closed',
+        },
+      };
+      const req = {
+        params: {
+          test: 'test',
+        },
+        query: {
+          ip: '127.0.3.1',
+        },
+      };
+      const generateResponse = () => {
+        const res = { test: 'testing' };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.fake((param) => param);
+        return res;
+      };
+      const res = generateResponse();
+
+      const result = await fluxCommunication.removePeer(req, res);
+
+      expect(result).to.eql(expectedResult);
+      sinon.assert.calledOnceWithExactly(verificationHelperStub, 'adminandfluxteam', req);
+    });
+
+    it('should issue a warning if a connection does not exist', async () => {
+      verificationHelperStub = sinon.stub(verificationHelper, 'verifyPrivilege').returns(true);
+      const expectedResult = {
+        status: 'warning',
+        data: {
+          code: undefined,
+          name: undefined,
+          message: 'Connection to 127.0.3.1 does not exists.',
+        },
+      };
+      const req = {
+        params: {
+          ip: '127.0.3.1',
+        },
+      };
+      const generateResponse = () => {
+        const res = { test: 'testing' };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.fake((param) => param);
+        return res;
+      };
+      const res = generateResponse();
+
+      const result = await fluxCommunication.removePeer(req, res);
+
+      expect(result).to.eql(expectedResult);
+      sinon.assert.calledOnceWithExactly(verificationHelperStub, 'adminandfluxteam', req);
+    });
+
+    it('should issue an error message if ip is not provided', async () => {
+      verificationHelperStub = sinon.stub(verificationHelper, 'verifyPrivilege').returns(true);
+      const expectedResult = {
+        status: 'error',
+        data: {
+          code: undefined,
+          name: undefined,
+          message: 'No IP address specified.',
+        },
+      };
+      const req = {
+        params: {
+          test: 'test1',
+        },
+        query: {
+          test2: 'test3',
+        },
+      };
+      const generateResponse = () => {
+        const res = { test: 'testing' };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.fake((param) => param);
+        return res;
+      };
+      const res = generateResponse();
+
+      const result = await fluxCommunication.removePeer(req, res);
+
+      expect(result).to.eql(expectedResult);
+      sinon.assert.neverCalledWith(verificationHelperStub);
+    });
+
+    it('should issue an error message if user is unauthorized', async () => {
+      verificationHelperStub = sinon.stub(verificationHelper, 'verifyPrivilege').returns(false);
+      const expectedResult = {
+        status: 'error',
+        data: {
+          code: 401,
+          name: 'Unauthorized',
+          message: 'Unauthorized. Access denied.',
+        },
+      };
+      const req = {
+        params: {
+          ip: '127.0.3.1',
+        },
+      };
+      const generateResponse = () => {
+        const res = { test: 'testing' };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.fake((param) => param);
+        return res;
+      };
+      const res = generateResponse();
+
+      const result = await fluxCommunication.removePeer(req, res);
+
+      expect(result).to.eql(expectedResult);
+      sinon.assert.calledOnceWithExactly(verificationHelperStub, 'adminandfluxteam', req);
+    });
   });
 });
