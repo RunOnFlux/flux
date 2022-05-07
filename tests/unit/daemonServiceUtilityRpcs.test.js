62d391266a2b4d0a68f4d04198f7892967a593bd
JacekAdamczyk <jacek.adamczyk@gmail.com>
2022-05-07 12:11:15 +0200

    utilityrpcs tests

---
+++ tests/unit/daemonServiceUtilityRpcs.test.js
@@ -0,0 +1,408 @@
+const sinon = require('sinon');
+const { PassThrough } = require('stream');
+const { expect } = require('chai');
+const daemonServiceUtils = require('../../ZelBack/src/services/daemonServiceUtils');
+const serviceHelper = require('../../ZelBack/src/services/serviceHelper');
+const daemonServiceUtilityRpcs = require('../../ZelBack/src/services/daemonServiceUtilityRpcs');
+
+const generateResponse = () => {
+  const res = { test: 'testing' };
+  res.status = sinon.stub().returns(res);
+  res.json = sinon.fake((param) => `Response: ${param}`);
+  return res;
+};
+
+describe.only('daemonServiceUtilityRpcs tests', () => {
+  describe('createMultiSig tests', () => {
+    let daemonServiceUtilsStub;
+
+    beforeEach(() => {
+      daemonServiceUtilsStub = sinon.stub(daemonServiceUtils, 'executeCall');
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should trigger rpc, no parameters, no response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          test: 'test',
+        },
+        query: {
+          test2: 'test2',
+        },
+      };
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.createMultiSig(req);
+
+      expect(result).to.equal(expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'createMultiSig', []);
+    });
+
+    it('should trigger rpc, response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          test: 'test',
+        },
+        query: {
+          test2: 'test2',
+        },
+      };
+      const res = generateResponse();
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.createMultiSig(req, res);
+
+      expect(result).to.equal(`Response: ${expectedResponse}`);
+      sinon.assert.calledOnceWithExactly(res.json, expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'createMultiSig', []);
+    });
+
+    it('should trigger rpc, data passed in params, no response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          n: '12345',
+          keys: {
+            key1: 'key1',
+            key2: 'key2',
+          },
+        },
+        query: {
+          test2: 'test2',
+        },
+      };
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.createMultiSig(req);
+
+      expect(result).to.equal(expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'createMultiSig', [+req.params.n, req.params.keys]);
+    });
+
+    it('should trigger rpc, no n param, no response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          keys: {
+            key1: 'key1',
+            key2: 'key2',
+          },
+        },
+        query: {
+          test2: 'test2',
+        },
+      };
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.createMultiSig(req);
+
+      expect(result).to.equal(expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'createMultiSig', []);
+    });
+
+    it('should trigger rpc, no keys param, no response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          n: '12345',
+        },
+        query: {
+          test2: 'test2',
+        },
+      };
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.createMultiSig(req);
+
+      expect(result).to.equal(expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'createMultiSig', []);
+    });
+
+    it('should trigger rpc, data passed in query, no response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          test: 'test',
+        },
+        query: {
+          n: '12345',
+          keys: {
+            key1: 'key1',
+            key2: 'key2',
+          },
+        },
+      };
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.createMultiSig(req);
+
+      expect(result).to.equal(expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'createMultiSig', [+req.query.n, req.query.keys]);
+    });
+  });
+
+  describe('createMultiSigPost tests', () => {
+    let daemonServiceUtilsStub;
+    const execCallResult = 'RPC call executed';
+
+    beforeEach(() => {
+      daemonServiceUtilsStub = sinon.stub(daemonServiceUtils, 'executeCall').resolves(execCallResult);
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should call createMultiSig if no params are given', async () => {
+      const params = {
+      };
+      const expectedCallParams = [];
+      const mockStream = new PassThrough();
+      mockStream.push(JSON.stringify(params));
+      mockStream.end();
+      const res = generateResponse();
+
+      await daemonServiceUtilityRpcs.createMultiSigPost(mockStream, res);
+      // await because of the async nature of the request processing
+      await serviceHelper.delay(150);
+
+      sinon.assert.calledOnceWithExactly(res.json, execCallResult);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'createMultiSig', expectedCallParams);
+    });
+
+    it('should call createMultiSig with all params', async () => {
+      const params = {
+        n: '12345',
+        keys: {
+          key1: 'key1',
+          key2: 'key2',
+        },
+      };
+      const mockStream = new PassThrough();
+      mockStream.push(JSON.stringify(params));
+      mockStream.end();
+      const res = generateResponse();
+
+      await daemonServiceUtilityRpcs.createMultiSigPost(mockStream, res);
+      // await because of the async nature of the request processing
+      await serviceHelper.delay(150);
+
+      sinon.assert.calledOnceWithExactly(res.json, execCallResult);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'createMultiSig', [+params.n, params.keys]);
+    });
+
+    it('should call createMultiSig with no keys param', async () => {
+      const params = {
+        n: '12345',
+      };
+      const mockStream = new PassThrough();
+      mockStream.push(JSON.stringify(params));
+      mockStream.end();
+      const res = generateResponse();
+
+      await daemonServiceUtilityRpcs.createMultiSigPost(mockStream, res);
+      // await because of the async nature of the request processing
+      await serviceHelper.delay(150);
+
+      sinon.assert.calledOnceWithExactly(res.json, execCallResult);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'createMultiSig', []);
+    });
+
+    it('should call rpc with no n param', async () => {
+      const params = {
+        keys: {
+          key1: 'key1',
+          key2: 'key2',
+        },
+      };
+      const mockStream = new PassThrough();
+      mockStream.push(JSON.stringify(params));
+      mockStream.end();
+      const res = generateResponse();
+
+      await daemonServiceUtilityRpcs.createMultiSigPost(mockStream, res);
+      // await because of the async nature of the request processing
+      await serviceHelper.delay(150);
+
+      sinon.assert.calledOnceWithExactly(res.json, execCallResult);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'createMultiSig', []);
+    });
+  });
+
+  describe('estimateFee tests', () => {
+    let daemonServiceUtilsStub;
+
+    beforeEach(() => {
+      daemonServiceUtilsStub = sinon.stub(daemonServiceUtils, 'executeCall');
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should trigger rpc, no parameters, no response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          test: 'test',
+        },
+        query: {
+          test2: 'test2',
+        },
+      };
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.estimateFee(req);
+
+      expect(result).to.equal(expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'estimateFee', []);
+    });
+
+    it('should trigger rpc, response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          test: 'test',
+        },
+        query: {
+          test2: 'test2',
+        },
+      };
+      const res = generateResponse();
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.estimateFee(req, res);
+
+      expect(result).to.equal(`Response: ${expectedResponse}`);
+      sinon.assert.calledOnceWithExactly(res.json, expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'estimateFee', []);
+    });
+
+    it('should trigger rpc, data passed in params, no response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          nblocks: '12345',
+        },
+        query: {
+          test2: 'test2',
+        },
+      };
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.estimateFee(req);
+
+      expect(result).to.equal(expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'estimateFee', [+req.params.nblocks]);
+    });
+
+    it('should trigger rpc, data passed in query, no response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          test: 'test',
+        },
+        query: {
+          nblocks: '12345',
+        },
+      };
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.estimateFee(req);
+
+      expect(result).to.equal(expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'estimateFee', [+req.query.nblocks]);
+    });
+  });
+
+  describe('estimatePriority tests', () => {
+    let daemonServiceUtilsStub;
+
+    beforeEach(() => {
+      daemonServiceUtilsStub = sinon.stub(daemonServiceUtils, 'executeCall');
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should trigger rpc, no parameters, no response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          test: 'test',
+        },
+        query: {
+          test2: 'test2',
+        },
+      };
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.estimatePriority(req);
+
+      expect(result).to.equal(expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'estimatePriority', []);
+    });
+
+    it('should trigger rpc, response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          test: 'test',
+        },
+        query: {
+          test2: 'test2',
+        },
+      };
+      const res = generateResponse();
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.estimatePriority(req, res);
+
+      expect(result).to.equal(`Response: ${expectedResponse}`);
+      sinon.assert.calledOnceWithExactly(res.json, expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'estimatePriority', []);
+    });
+
+    it('should trigger rpc, data passed in params, no response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          nblocks: '12345',
+        },
+        query: {
+          test2: 'test2',
+        },
+      };
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.estimatePriority(req);
+
+      expect(result).to.equal(expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'estimatePriority', [+req.params.nblocks]);
+    });
+
+    it('should trigger rpc, data passed in query, no response passed', async () => {
+      daemonServiceUtilsStub.returns('success');
+      const req = {
+        params: {
+          test: 'test',
+        },
+        query: {
+          nblocks: '12345',
+        },
+      };
+      const expectedResponse = 'success';
+
+      const result = await daemonServiceUtilityRpcs.estimatePriority(req);
+
+      expect(result).to.equal(expectedResponse);
+      sinon.assert.calledOnceWithExactly(daemonServiceUtilsStub, 'estimatePriority', [+req.query.nblocks]);
+    });
+  });
+});
