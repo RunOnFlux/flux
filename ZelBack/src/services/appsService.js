008bfef88e0174c41a5058f9664fb078b2e1ca66
Cabecinha84 <42519726+Cabecinha84@users.noreply.github.com>
2022-05-25 11:03:58 +0100

    broadcast running app after global install

--- ZelBack/src/services/appsService.js
+++ ZelBack/src/services/appsService.js
@@ -6350,6 +6350,21 @@ async function trySpawningGlobalApplication() {
     // install the app
     await registerAppLocally(appSpecifications); // can throw
 
+    const broadcastedAt = new Date().getTime();
+    const newAppRunningMessage = {
+      type: 'fluxapprunning',
+      version: 1,
+      hash: appSpecifications.hash, // hash of application specifics that are running
+      ip: myIP,
+      broadcastedAt,
+    };
+
+    // broadcast messages about running apps to all peers
+    await fluxCommunicationMessagesSender.broadcastMessageToOutgoing(newAppRunningMessage);
+    await serviceHelper.delay(100);
+    await fluxCommunicationMessagesSender.broadcastMessageToIncoming(newAppRunningMessage);
+    // broadcast messages about running apps to all peers
+
     await serviceHelper.delay(10 * config.fluxapps.installation.delay * 1000);
     log.info('Reinitiating possible app installation');
     trySpawningGlobalApplication();
