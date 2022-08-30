c5fceef91797ea53b66aa337453de8f5c26cf8d3
SyGem Limited <simon@sygem.com>
2022-08-30 14:23:43 +0100

    Highlight the fee on the redeem dialog

--- HomeUI/src/views/apps/marketplace/SharedNodesView.vue
+++ HomeUI/src/views/apps/marketplace/SharedNodesView.vue
@@ -850,7 +850,7 @@
               {{ toFixedLocaleString(totalReward - (titanConfig ? titanConfig.redeemFee : 0), 2) }} Flux
             </h3>
             <h6>
-              (Redeem Fee: {{ titanConfig ? titanConfig.redeemFee : '...' }} Flux)
+              (<span class="text-warning">Redeem Fee:</span> <span class="text-danger">{{ titanConfig ? titanConfig.redeemFee : '...' }} Flux</span>)
             </h6>
           </b-card>
         </tab-content>
