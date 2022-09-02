70bada6758f1fd80230729284af36ace75ef7d14
JacekAdamczyk <jacek.adamczyk@gmail.com>
2022-09-02 11:49:41 +0200

    finish up tests

--- tests/unit/upnpService.test.js
+++ tests/unit/upnpService.test.js
@@ -5,6 +5,7 @@ const natUpnp = require('@runonflux/nat-upnp');
 const sinon = require('sinon');
 const proxyquire = require('proxyquire');
 const log = require('../../ZelBack/src/lib/log');
+const verificationHelper = require('../../ZelBack/src/services/verificationHelper');
 
 const { expect } = chai;
 
@@ -12,10 +13,21 @@ const config = {
   apiport: '5550',
 };
 
+const generateResponse = () => {
+  const res = { test: 'testing' };
+  res.status = sinon.stub().returns(res);
+  res.json = sinon.fake((param) => `Response: ${param}`);
+  res.write = sinon.fake(() => 'written');
+  res.end = sinon.fake(() => true);
+  res.writeHead = sinon.fake(() => true);
+  res.download = sinon.fake(() => true);
+  return res;
+};
+
 const upnpService = proxyquire('../../ZelBack/src/services/upnpService',
   { config });
 
-describe.only('upnpService tests', () => {
+describe('upnpService tests', () => {
   describe('verifyUPNPsupport tests', () => {
     let logSpy;
 
@@ -188,10 +200,18 @@ describe.only('upnpService tests', () => {
       sinon.assert.notCalled(logSpy);
       sinon.assert.calledTwice(createMappingSpy);
       sinon.assert.calledWithExactly(createMappingSpy, {
-        public: 123, private: 123, ttl: 0, description: 'Flux_Backend_API',
+        public: 123,
+        private: 123,
+        ttl: 0,
+        protocol: 'TCP',
+        description: 'some description',
       });
       sinon.assert.calledWithExactly(createMappingSpy, {
-        public: 122, private: 122, ttl: 0, description: 'Flux_Home_UI',
+        public: 123,
+        private: 123,
+        ttl: 0,
+        protocol: 'UDP',
+        description: 'some description',
       });
     });
 
@@ -202,6 +222,532 @@ describe.only('upnpService tests', () => {
 
       expect(result).to.equal(false);
       sinon.assert.calledOnce(logSpy);
+    });
+  });
+
+  describe('removeMapUpnpPort tests', () => {
+    let logSpy;
+    let removeMappingSpy;
+
+    beforeEach(() => {
+      logSpy = sinon.spy(log, 'error');
+      removeMappingSpy = sinon.stub(natUpnp.Client.prototype, 'removeMapping');
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should return true if all client responses are valid', async () => {
+      removeMappingSpy.returns(true);
+
+      const result = await upnpService.removeMapUpnpPort(123, 'some description');
+
+      expect(result).to.equal(true);
+      sinon.assert.notCalled(logSpy);
+      sinon.assert.calledTwice(removeMappingSpy);
+      sinon.assert.calledWithExactly(removeMappingSpy, { public: 123, protocol: 'TCP' });
+      sinon.assert.calledWithExactly(removeMappingSpy, { public: 123, protocol: 'UDP' });
+    });
+
+    it('should return error if client response throws', async () => {
+      removeMappingSpy.throws();
+
+      const result = await upnpService.removeMapUpnpPort(123);
+
+      expect(result).to.equal(false);
+      sinon.assert.calledOnce(logSpy);
+    });
+  });
+
+  describe('mapPortApi tests', () => {
+    let verifyPrivilegeStub;
+    let createMappingSpy;
+
+    beforeEach(async () => {
+      verifyPrivilegeStub = sinon.stub(verificationHelper, 'verifyPrivilege');
+      createMappingSpy = sinon.stub(natUpnp.Client.prototype, 'createMapping');
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should create error message if user does not have proper access', async () => {
+      verifyPrivilegeStub.resolves(false);
+      const res = generateResponse();
+
+      await upnpService.mapPortApi(undefined, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'error',
+        data: {
+          code: 401,
+          name: 'Unauthorized',
+          message: 'Unauthorized. Access denied.',
+        },
+      });
+    });
+
+    it('should throw error if port is null', async () => {
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        params: {
+          port: null,
+        },
+        query: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.mapPortApi(req, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'error',
+        data: {
+          code: undefined,
+          name: 'Error',
+          message: 'No Port address specified.',
+        },
+      });
+    });
+
+    it('should throw error if port is undefined', async () => {
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        params: {
+          test: 'test2',
+        },
+        query: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.mapPortApi(req, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'error',
+        data: {
+          code: undefined,
+          name: 'Error',
+          message: 'No Port address specified.',
+        },
+      });
+    });
+
+    it('should show a proper message if port is given in the params', async () => {
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        params: {
+          port: '1234',
+        },
+        query: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.mapPortApi(req, res);
+
+      sinon.assert.calledWithExactly(createMappingSpy, {
+        public: 1234,
+        private: 1234,
+        ttl: 0,
+        protocol: 'TCP',
+        description: 'Flux_manual_entry',
+      });
+      sinon.assert.calledWithExactly(createMappingSpy, {
+        public: 1234,
+        private: 1234,
+        ttl: 0,
+        protocol: 'UDP',
+        description: 'Flux_manual_entry',
+      });
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'success',
+        data: { code: undefined, name: undefined, message: 'Port mapped' },
+      });
+    });
+
+    it('should show a proper message if port is given in the query', async () => {
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        query: {
+          port: '1234',
+        },
+        params: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.mapPortApi(req, res);
+      sinon.assert.calledWithExactly(createMappingSpy, {
+        public: 1234,
+        private: 1234,
+        ttl: 0,
+        protocol: 'TCP',
+        description: 'Flux_manual_entry',
+      });
+      sinon.assert.calledWithExactly(createMappingSpy, {
+        public: 1234,
+        private: 1234,
+        ttl: 0,
+        protocol: 'UDP',
+        description: 'Flux_manual_entry',
+      });
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'success',
+        data: { code: undefined, name: undefined, message: 'Port mapped' },
+      });
+    });
+  });
+
+  describe('removeMapPortApi tests', () => {
+    let verifyPrivilegeStub;
+    let removeMappingSpy;
+
+    beforeEach(async () => {
+      verifyPrivilegeStub = sinon.stub(verificationHelper, 'verifyPrivilege');
+      removeMappingSpy = sinon.stub(natUpnp.Client.prototype, 'removeMapping');
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should create error message if user does not have proper access', async () => {
+      verifyPrivilegeStub.resolves(false);
+      const res = generateResponse();
+
+      await upnpService.removeMapPortApi(undefined, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'error',
+        data: {
+          code: 401,
+          name: 'Unauthorized',
+          message: 'Unauthorized. Access denied.',
+        },
+      });
+    });
+
+    it('should throw error if port is null', async () => {
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        params: {
+          port: null,
+        },
+        query: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.removeMapPortApi(req, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'error',
+        data: {
+          code: undefined,
+          name: 'Error',
+          message: 'No Port address specified.',
+        },
+      });
+    });
+
+    it('should throw error if port is undefined', async () => {
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        params: {
+          test: 'test2',
+        },
+        query: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.removeMapPortApi(req, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'error',
+        data: {
+          code: undefined,
+          name: 'Error',
+          message: 'No Port address specified.',
+        },
+      });
+    });
+
+    it('should show a proper message if port is given in the params', async () => {
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        params: {
+          port: '1234',
+        },
+        query: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.removeMapPortApi(req, res);
+
+      sinon.assert.calledWithExactly(removeMappingSpy, {
+        public: 1234,
+        protocol: 'UDP',
+      });
+      sinon.assert.calledWithExactly(removeMappingSpy, {
+        public: 1234,
+        protocol: 'TCP',
+      });
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'success',
+        data: { code: undefined, name: undefined, message: 'Port unmapped' },
+      });
+    });
+
+    it('should show a proper message if port is given in the query', async () => {
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        query: {
+          port: '1234',
+        },
+        params: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.removeMapPortApi(req, res);
+
+      sinon.assert.calledWithExactly(removeMappingSpy, {
+        public: 1234,
+        protocol: 'TCP',
+      });
+      sinon.assert.calledWithExactly(removeMappingSpy, {
+        public: 1234,
+        protocol: 'UDP',
+      });
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'success',
+        data: { code: undefined, name: undefined, message: 'Port unmapped' },
+      });
+    });
+  });
+
+  describe('getMapApi tests', () => {
+    let verifyPrivilegeStub;
+    let getMappingsSpy;
+
+    beforeEach(async () => {
+      verifyPrivilegeStub = sinon.stub(verificationHelper, 'verifyPrivilege');
+      getMappingsSpy = sinon.stub(natUpnp.Client.prototype, 'getMappings');
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should create error message if user does not have proper access', async () => {
+      verifyPrivilegeStub.resolves(false);
+      const res = generateResponse();
+
+      await upnpService.getMapApi(undefined, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'error',
+        data: {
+          code: 401,
+          name: 'Unauthorized',
+          message: 'Unauthorized. Access denied.',
+        },
+      });
+    });
+
+    it('should create error message if getMappings throws', async () => {
+      getMappingsSpy.throws();
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        params: {
+          test: 'test2',
+        },
+        query: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.getMapApi(req, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'error',
+        data: { code: undefined, name: 'Error', message: 'Error' },
+      });
+    });
+
+    it('should show a proper message if all data is valid', async () => {
+      getMappingsSpy.resolves({
+        data: 'data1',
+      });
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        params: {
+          port: '1234',
+        },
+        query: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.getMapApi(req, res);
+
+      sinon.assert.calledOnce(getMappingsSpy);
+      sinon.assert.calledOnceWithExactly(res.json, { status: 'success', data: { data: 'data1' } });
+    });
+  });
+
+  describe('getIpApi tests', () => {
+    let verifyPrivilegeStub;
+    let getPublicIpSpy;
+
+    beforeEach(async () => {
+      verifyPrivilegeStub = sinon.stub(verificationHelper, 'verifyPrivilege');
+      getPublicIpSpy = sinon.stub(natUpnp.Client.prototype, 'getPublicIp');
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should create error message if user does not have proper access', async () => {
+      verifyPrivilegeStub.resolves(false);
+      const res = generateResponse();
+
+      await upnpService.getIpApi(undefined, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'error',
+        data: {
+          code: 401,
+          name: 'Unauthorized',
+          message: 'Unauthorized. Access denied.',
+        },
+      });
+    });
+
+    it('should create error message if getMappings throws', async () => {
+      getPublicIpSpy.throws();
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        params: {
+          test: 'test2',
+        },
+        query: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.getIpApi(req, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'error',
+        data: { code: undefined, name: 'Error', message: 'Error' },
+      });
+    });
+
+    it('should show a proper message if all data is valid', async () => {
+      getPublicIpSpy.resolves('192.169.1.1');
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        params: {
+          port: '1234',
+        },
+        query: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.getIpApi(req, res);
+
+      sinon.assert.calledOnce(getPublicIpSpy);
+      sinon.assert.calledOnceWithExactly(res.json, { status: 'success', data: '192.169.1.1' });
+    });
+  });
+
+  describe('getGatewayApi tests', () => {
+    let verifyPrivilegeStub;
+    let getGatewaySpy;
+
+    beforeEach(async () => {
+      verifyPrivilegeStub = sinon.stub(verificationHelper, 'verifyPrivilege');
+      getGatewaySpy = sinon.stub(natUpnp.Client.prototype, 'getGateway');
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should create error message if user does not have proper access', async () => {
+      verifyPrivilegeStub.resolves(false);
+      const res = generateResponse();
+
+      await upnpService.getGatewayApi(undefined, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'error',
+        data: {
+          code: 401,
+          name: 'Unauthorized',
+          message: 'Unauthorized. Access denied.',
+        },
+      });
+    });
+
+    it('should create error message if getMappings throws', async () => {
+      getGatewaySpy.throws();
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        params: {
+          test: 'test2',
+        },
+        query: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.getGatewayApi(req, res);
+
+      sinon.assert.calledOnceWithExactly(res.json, {
+        status: 'error',
+        data: { code: undefined, name: 'Error', message: 'Error' },
+      });
+    });
+
+    it('should show a proper message if all data is valid', async () => {
+      getGatewaySpy.resolves('10.1.1.1');
+      verifyPrivilegeStub.resolves(true);
+      const req = {
+        params: {
+          port: '1234',
+        },
+        query: {
+          test: 'test',
+        },
+      };
+      const res = generateResponse();
+
+      await upnpService.getGatewayApi(req, res);
+
+      sinon.assert.calledOnce(getGatewaySpy);
+      sinon.assert.calledOnceWithExactly(res.json, { status: 'success', data: '10.1.1.1' });
     });
   });
 });
