ea347c1c45a9a5cf6126d6836bc2c0df82dedf7b
JacekAdamczyk <jacek.adamczyk@gmail.com>
2022-09-17 13:11:17 +0200

    add explorer service tests

--- tests/unit/explorerService.test.js
+++ tests/unit/explorerService.test.js
@@ -133,4 +133,97 @@ describe.only('explorerService tests', () => {
       sinon.assert.notCalled(lruStubSet);
     });
   });
+
+  describe.only('getSenderForFluxTx tests', () => {
+    let dbStub;
+    let lruStubGet;
+    let lruStubSet;
+    const txid = '12345';
+    const vout = '444';
+    let daemonServiceTransactionRpcsStub;
+
+    beforeEach(async () => {
+      lruStubGet = sinon.stub(LRU.prototype, 'get');
+      lruStubSet = sinon.stub(LRU.prototype, 'set');
+      dbStub = sinon.stub(dbHelper, 'findOneInDatabase');
+      await dbHelper.initiateDB();
+      dbHelper.databaseConnection();
+      daemonServiceTransactionRpcsStub = sinon.stub(daemonServiceTransactionRpcs, 'getRawTransaction');
+    });
+
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    it('should return true if node cache exists', async () => {
+      lruStubGet.returns(true);
+
+      const result = await explorerService.getSenderForFluxTxInsight(txid, vout);
+
+      expect(result).to.equal(true);
+      sinon.assert.calledOnceWithExactly(lruStubGet, `${txid}-${vout}`);
+    });
+
+    it('should set cache and return sender if there is an entry in the db', async () => {
+      const txContent = {
+        txid,
+        address: 12345,
+        satoshis: 10000,
+      };
+      lruStubGet.returns(false);
+      dbStub.returns(txContent);
+
+      const result = await explorerService.getSenderForFluxTxInsight(txid, vout);
+
+      expect(result).to.equal(txContent);
+      sinon.assert.calledOnceWithExactly(lruStubGet, `${txid}-${vout}`);
+      sinon.assert.calledOnceWithExactly(lruStubSet, `${txid}-${vout}`, txContent);
+    });
+
+    it('should adjustedTxContent if there is no entry in the db', async () => {
+      const txContent = {
+        txid,
+        vout: [{
+          n: 444,
+          scriptPubKey:
+          { addresses: ['1ZACDE1234567'] },
+          valueSat: 1000,
+        }],
+      };
+      lruStubGet.returns(false);
+      dbStub.returns(false);
+      daemonServiceTransactionRpcsStub.returns({
+        status: 'success',
+        data: { ...txContent },
+      });
+      const result = await explorerService.getSenderForFluxTxInsight(txid, vout);
+
+      expect(result).to.eql({ txid: '12345', address: '1ZACDE1234567', satoshis: 1000 });
+      sinon.assert.calledOnceWithExactly(lruStubGet, `${txid}-${vout}`);
+      sinon.assert.calledOnceWithExactly(lruStubSet, `${txid}-${vout}`, { txid: '12345', address: '1ZACDE1234567', satoshis: 1000 });
+    });
+
+    it('should adjustedTxContent if there is no entry in the db, transactionOutput not found', async () => {
+      const txContent = {
+        txid,
+        vout: [{
+          n: 443,
+          scriptPubKey:
+          { addresses: ['1ZACDE1234567'] },
+          valueSat: 1000,
+        }],
+      };
+      lruStubGet.returns(false);
+      dbStub.returns(false);
+      daemonServiceTransactionRpcsStub.returns({
+        status: 'success',
+        data: { ...txContent },
+      });
+      const result = await explorerService.getSenderForFluxTxInsight(txid, vout);
+
+      expect(result).to.eql({ txid: '12345', address: undefined, satoshis: undefined });
+      sinon.assert.calledOnceWithExactly(lruStubGet, `${txid}-${vout}`);
+      sinon.assert.notCalled(lruStubSet);
+    });
+  });
 });
