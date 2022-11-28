2413a9f797e7edbdabc77c2eabbe3e2dcb96c41f
Kyuubi2709 <89035963+Kyuubi2709@users.noreply.github.com>
2022-11-28 12:00:50 +0100

    Update marketplaceCategories.js
    I added two new categories. 
    
    1. Front-end
    2. Blockbook

--- HomeUI/src/libs/marketplaceCategories.js
+++ HomeUI/src/libs/marketplaceCategories.js
@@ -3,6 +3,8 @@ const categories = [
   { name: 'Productivity', variant: 'danger', icon: 'file-alt' },
   { name: 'Hosting', variant: 'success', icon: 'server' },
   { name: 'Crypto', variant: 'success', icon: 'coins' },
+  { name: 'Blockbook', variant: 'success', icon: 'server' },
+  { name: 'Front-end', variant: 'success', icon: 'coins' },
 ];
 const defaultCategory = { name: 'App', variant: 'success', icon: 'cog' };
 
