cd7240742fd8f2abd8ec02e1331dede6224e8a4f
TheTrunk <tadeas.kmenta@zel.cash>
2023-01-07 10:16:59 +0700

    correct for start, end, chaininfo

--- ZelBack/src/services/daemonService/daemonServiceAddressRpcs.js
+++ ZelBack/src/services/daemonService/daemonServiceAddressRpcs.js
@@ -50,9 +50,14 @@ async function getSingleAddresssTxids(req, res) {
 
   const options = {
     addresses: [address],
-    start,
-    end,
   };
+
+  if (start) {
+    options.start = serviceHelper.ensureNumber(start);
+  }
+  if (end) {
+    options.end = serviceHelper.ensureNumber(end);
+  }
 
   const rpccall = 'getaddresstxids';
   const rpcparameters = [options];
@@ -163,10 +168,18 @@ async function getSingleAddressDeltas(req, res) {
 
   const options = {
     addresses: [address],
-    start,
-    end,
-    chainInfo: chaininfo,
   };
+
+  if (start) {
+    options.start = serviceHelper.ensureNumber(start);
+  }
+  if (end) {
+    options.end = serviceHelper.ensureNumber(end);
+  }
+
+  if (chaininfo) {
+    options.chainInfo = serviceHelper.ensureBoolean(chaininfo);
+  }
 
   const rpccall = 'getaddressdeltas';
   const rpcparameters = [options];
