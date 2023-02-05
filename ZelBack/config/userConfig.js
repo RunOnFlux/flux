62aeb484b76c3a9ec9747c7c7dabc0d40b68eeb1
johnhanlon86 <johnhanlon86@gmail.com>
2023-02-05 20:10:34 +0000

    Update userConfig

--- ZelBack/config/userConfig.js
+++ ZelBack/config/userConfig.js
@@ -1,6 +1,6 @@
 export const initial = {
   ipaddress: '94.23.39.36',
-  zelid: '143DoT2eg1nYyF54ftrP9kGHLBcZgB6WUx',
+  zelid: '1CbErtneaX2QVyUfwU7JGB7VzvPgrgc3uC',
   kadena: '',
   testnet: false,
   apiport: 16127
