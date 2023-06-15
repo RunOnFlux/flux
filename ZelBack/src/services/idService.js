fa2798690ceda1760072e5840fcdd244aa2c7a58
Cabecinha84 <valter.cabecinha@gmail.com>
2023-06-15 11:42:08 +0100

    Remove restart syncthing from loginPhrase as startSyncthing executes every few minutes and already applies a restart if needed

--- ZelBack/src/services/idService.js
+++ ZelBack/src/services/idService.js
@@ -109,7 +109,6 @@ async function loginPhrase(req, res) {
     if (syncthingDeviceID.status === 'error') {
       if (syncthingWorking) {
         syncthingWorking = false;
-        syncthingService.systemRestart();
       } else {
         throw new Error('Syncthing is not running properly');
       }
