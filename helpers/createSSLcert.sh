3db3f64933198850461bf33feb4b2c5bf86ea82d
TheTrunk <tadeas.kmenta@zel.cash>
2023-08-11 10:09:00 +0200

    create root signign cert

--- helpers/createSSLcert.sh
+++ helpers/createSSLcert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ../certs/v1.key -out ../certs/v1.crt -subj '/CN=self.api.runonflux.io/O=RunOnFlux' > /dev/null 2>&1
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -CA ../certs/v1rootcert.cert.pem -CAkey ../certs/v1rootcert.privkey.pem -keyout ../certs/v1.key -out ../certs/v1.crt -subj '/CN=self.api.runonflux.io/O=RunOnFlux' > /dev/null 2>&1
 sleep 2
