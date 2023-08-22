5e1338e7020bdfb0b3d4df664e51a434e7e3891f
TheTrunk <tadeas.kmenta@zel.cash>
2023-08-22 14:38:20 +0200

    fix tests

--- tests/unit/upnpService.test.js
+++ tests/unit/upnpService.test.js
@@ -148,10 +148,10 @@ describe('upnpService tests', () => {
         public: 123, private: 123, ttl: 0, description: 'Flux_Backend_API',
       });
       sinon.assert.calledWithExactly(createMappingSpy, {
-        public: 122, private: 122, ttl: 0, description: 'Flux_Home_UI',
+        public: 124, private: 124, ttl: 0, description: 'Flux_Backend_API_SSL',
       });
       sinon.assert.calledWithExactly(createMappingSpy, {
-        public: 124, private: 124, ttl: 0, description: 'Flux_Backend_API_SSL',
+        public: 122, private: 122, ttl: 0, description: 'Flux_Home_UI',
       });
       sinon.assert.calledWithExactly(createMappingSpy, {
         public: 125, private: 125, ttl: 0, description: 'Flux_Syncthing',
@@ -170,10 +170,10 @@ describe('upnpService tests', () => {
         public: 16127, private: 16127, ttl: 0, description: 'Flux_Backend_API',
       });
       sinon.assert.calledWithExactly(createMappingSpy, {
-        public: 16126, private: 16126, ttl: 0, description: 'Flux_Home_UI',
+        public: 16128, private: 16128, ttl: 0, description: 'Flux_Backend_API_SSL',
       });
       sinon.assert.calledWithExactly(createMappingSpy, {
-        public: 16128, private: 16128, ttl: 0, description: 'Flux_Backend_API_SSL',
+        public: 16126, private: 16126, ttl: 0, description: 'Flux_Home_UI',
       });
       sinon.assert.calledWithExactly(createMappingSpy, {
         public: 16129, private: 16129, ttl: 0, description: 'Flux_Syncthing',
