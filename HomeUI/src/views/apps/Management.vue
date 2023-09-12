99b9ada458e037fdc3b266b07d845ede6d69adb4
Cabecinha84 <valter.cabecinha@gmail.com>
2023-09-12 15:33:08 +0100

    Enable Dev marketplace

--- HomeUI/src/views/apps/Management.vue
+++ HomeUI/src/views/apps/Management.vue
@@ -3832,6 +3832,7 @@ const openpgp = require('openpgp');
 const timeoptions = require('@/libs/dateFormat');
 
 const geolocations = require('../../libs/geolocation');
+const userconfig = require('../../../../config/userconfig');
 
 export default {
   components: {
@@ -4323,7 +4324,12 @@ export default {
     },
     async getMarketPlace() {
       try {
-        const response = await axios.get('https://stats.runonflux.io/marketplace/listapps');
+        let marketPlaceUrl = 'https://stats.runonflux.io/marketplace/listapps';
+        const developmentNode = userconfig.initial.development || false;
+        if (developmentNode) {
+          marketPlaceUrl = 'https://stats.runonflux.io/marketplace/listdevapps';
+        }
+        const response = await axios.get(marketPlaceUrl);
         if (response.data.status === 'success') {
           this.marketPlaceApps = response.data.data;
         }
