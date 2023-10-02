229dd9b0fc006587210723449828b661e1601bf2
Cabecinha84 <valter.cabecinha@gmail.com>
2023-10-02 21:52:17 +0200

    added new banned port

--- ZelBack/src/services/appsService.js
+++ ZelBack/src/services/appsService.js
@@ -9998,7 +9998,7 @@ async function signCheckAppData(message) {
 /**
  * Periodically check for our applications port range is available
 */
-let testingPort = 83;
+let testingPort = 84;
 let failedPort;
 async function checkMyAppsAvailability() {
   const isUPNP = upnpService.isUPNP();
