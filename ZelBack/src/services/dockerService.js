6aeb4087d5f49d85a27b6d6c4600df60b8b4a28a
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-10-19 08:36:24 +0200

    fix data slice

--- ZelBack/src/services/dockerService.js
+++ ZelBack/src/services/dockerService.js
@@ -314,17 +314,13 @@ async function dockerContainerExec(container, cmd, env, res, callback) {
       Detach: false,
       Tty: false,
     };
-    let removedFirst8Bytes = false;
     const exec = await container.exec(options);
     exec.start(optionsExecStart, (err, mystream) => {
       if (err) {
         callback(err);
       }
       mystream.on('data', (data) => {
-        if (!removedFirst8Bytes) {
-          data = data.slice(8);
-          removedFirst8Bytes = true;
-        }
+        data = data.slice(8);
         res.write(data);
       });
       mystream.on('end', () => callback(null));
