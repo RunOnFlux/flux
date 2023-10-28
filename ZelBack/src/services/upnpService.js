c090d970a64cfdb4fe51846843f3790ec9957da5
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-10-28 18:31:31 +0200

    Update upnpService.js

--- ZelBack/src/services/upnpService.js
+++ ZelBack/src/services/upnpService.js
@@ -1,3 +1,5 @@
+/* global userconfig */
+/* eslint no-undef: "error" */
 const config = require('config');
 const natUpnp = require('@runonflux/nat-upnp');
 const serviceHelper = require('./serviceHelper');
