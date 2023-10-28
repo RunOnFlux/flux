421efd10be2b8570061bf6992d1204ebbd66102f
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-10-29 01:31:15 +0200

    fix eslint

--- tests/ZelBack/daemonService.js
+++ tests/ZelBack/daemonService.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 /* global userconfig */
-global.userconfig = require('../../config/userconfig') // eslint-disable-line
+global.userconfig = require('../../config/userconfig');
 process.env.NODE_CONFIG_DIR = `${process.cwd()}/ZelBack/config/`;
 const axios = require('axios');
 const chai = require('chai');
