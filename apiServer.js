b8a26048ab146f24fd567f4cd33b11ae2765dfc8
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-10-29 01:11:49 +0200

    fix eslint

--- apiServer.js
+++ apiServer.js
@@ -1,8 +1,8 @@
 /* global userconfig */
-/* eslint no-undef: "error" */
-// Flux configuration
+global.userconfig = require('./config/userconfig');
+
 process.env.NODE_CONFIG_DIR = `${__dirname}/ZelBack/config/`;
-userconfig = require('./config/userconfig');
+
 const config = require('config');
 const fs = require('fs');
 const https = require('https');
@@ -13,39 +13,41 @@ const app = require('./ZelBack/src/lib/server');
 const log = require('./ZelBack/src/lib/log');
 const serviceManager = require('./ZelBack/src/services/serviceManager');
 const upnpService = require('./ZelBack/src/services/upnpService');
-const cmdAsync = util.promisify(nodecmd.get);
+const hash = require('object-hash');
+const { watch } = require("fs/promises");
 
+const cmdAsync = util.promisify(nodecmd.get);
 const apiPort = userconfig.initial.apiport || config.server.apiport;
 const apiPortHttps = apiPort + 1;
 
-const hash = require('object-hash');
-const { watch } = require("fs/promises");
 let hashPrevious = null;
 let initialHash = null;
 
 async function configReload() {
   try {
     const watcher = watch(path.join(__dirname, '/config'));
+    // eslint-disable-next-line
     for await (const event of watcher) {
-      if ( event.eventType === 'change' && event.filename === 'userconfig.js') {
+      if (event.eventType === 'change' && event.filename === 'userconfig.js') {
         const hashCurrent = hash(fs.readFileSync(path.join(__dirname, '/config/userconfig.js')));
         if (hashCurrent === hashPrevious) {
           return;
         }
-        hashPrevious = hashCurrent
-        if( initialHash === null) {
-          initialHash = hashCurrent
+        hashPrevious = hashCurrent;
+        if (initialHash === null) {
+          initialHash = hashCurrent;
         }
-        if ( initialHash !== hashCurrent ) {
-          initialHash = null
+        if (initialHash !== hashCurrent) {
+          initialHash = null;
           log.info(`Config file changed, reloading ${event.filename}...`);
         }
         delete require.cache[require.resolve('./config/userconfig')];
+        // eslint-disable-next-line
         userconfig = require('./config/userconfig');
       }
     }
   } catch (error) {
-      log.error(`Error watching files: ${error}`);
+    log.error(`Error watching files: ${error}`);
   }
 }
 
