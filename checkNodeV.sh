15db24cf151599cf93cf987ab2f24382f94150e0
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-10-29 23:32:13 +0100

    added .npmrc

--- checkNodeV.sh
+++ checkNodeV.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 current_node_ver="$(node -v)"
 required_node_ver="v17.0.0"
- if [ "$(printf '%s\n' "$required_node_ver" "$current_node_ver" | sort -V | head -n1)" = "$required_node_ver" ]; then 
-       echo "Using NodeJS ${current_node_ver}. Adjust ENV to allow for NodeJS ${required_node_ver} or greater."
+ if [ "$(printf '%s\n' "$required_node_ver" "$current_node_ver" | sort -V | head -n1)" = "$required_node_ver">
+       echo "node-options='--openssl-legacy-provider'" > .npmrc
  else
-       echo "Using NodeJS ${current_node_ver}."
+       echo '' > .npmrc
  fi
