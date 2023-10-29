0b43013403055eb3a21dc36b278a1b5147b26d9d
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-10-29 23:40:06 +0100

    fix checkNodeV.sh

--- checkNodeV.sh
+++ checkNodeV.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 current_node_ver="$(node -v)"
 required_node_ver="v17.0.0"
- if [ "$(printf '%s\n' "$required_node_ver" "$current_node_ver" | sort -V | head -n1)" = "$required_node_ver">
+ if [ "$(printf '%s\n' "$required_node_ver" "$current_node_ver" | sort -V | head -n1)" = "$required_node_ver" ]; then
        echo "node-options='--openssl-legacy-provider'" > .npmrc
  else
        echo '' > .npmrc
