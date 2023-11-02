55daa5e00eda82561f599651a9f453f3da542c80
Cabecinha84 <valter.cabecinha@gmail.com>
2023-11-02 11:27:07 +0000

    fix typo

--- ZelBack/src/services/fluxNetworkHelper.js
+++ ZelBack/src/services/fluxNetworkHelper.js
@@ -195,7 +195,7 @@ function isPortUPNPBanned(port) {
  */
 async function isPortOpen(ip, port) {
   try {
-    // requirest netcat tool
+    // require netcat tool
     const exec = `nc -w 5 -z -v ${ip} ${port} </dev/null; echo $?`;
     const cmdAsync = util.promisify(nodecmd.get);
     const result = await cmdAsync(exec);
