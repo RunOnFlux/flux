29216a8ae66d630acf217ef0ab1d22e13ae6e515
David White <david.white@trustpower.co.nz>
2023-12-06 15:50:20 +0000

    Fix UPnP check to match current implementatio
    If apiPort is set and it's not 16127 - it's a UPnP node
    If apiPort is set and it's 16127 and routerIP is set - it's UPnP
    if apiPort is set and it's 16127 and no routerIP set - It's normal
    If no apiPort is set and no RouterIP is set - it's normal

--- apiServer.js
+++ apiServer.js
@@ -23,8 +23,12 @@ const apiPortHttps = +apiPort + 1;
 let initialHash = hash(fs.readFileSync(path.join(__dirname, '/config/userconfig.js')));
 
 async function loadUpnpIfRequired() {
+  // direct node (non UPnP)
+  if (userconfig.initial.apiport === config.server.apiport && !userconfig.initial.routerIP) {
+    return;
+  }
   // User configured UPnP node
-  if (userconfig.initial.apiport || userconfig.initial.routerIP) {
+  if (userconfig.initial.apiport) {
     if (!userconfig.initial.apiport || !userconfig.initial.routerIP) {
       log.error(`Flux UPnP port and RouterIP must both be set if running UPnP. Apiport: ${userconfig.initial.apiport}, RouterIP: ${userconfig.initial.routerIP}. Shutting down.`);
       process.exit();
