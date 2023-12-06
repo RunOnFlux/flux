6736bb6e9a9ffa447c042404c8b383dcf3fc8e19
David White <david.white@trustpower.co.nz>
2023-12-06 11:33:27 +0000

    Streamline app start
    The current app.js and apiserver.js are essentially duplicates.
    This commit removes a lot of that duplication. Also simplifies
    the way UPnP is loaded, and retests UPnP on every config reload.

--- app.js
+++ app.js
@@ -1,118 +1,47 @@
-/* global userconfig */
-global.userconfig = require('./config/userconfig');
-
 process.env.NODE_CONFIG_DIR = `${__dirname}/ZelBack/config/`;
-// Flux configuration
-const config = require('config');
-const compression = require('compression');
-const fs = require('fs');
-const https = require('https');
-const path = require('path');
-const util = require('util');
-const nodecmd = require('node-cmd');
-const express = require('express');
-const app = require('./ZelBack/src/lib/server');
+
+const { parseArgs } = require('node:util');
 const log = require('./ZelBack/src/lib/log');
-const socket = require('./ZelBack/src/lib/socket');
-const serviceManager = require('./ZelBack/src/services/serviceManager');
-const upnpService = require('./ZelBack/src/services/upnpService');
-const hash = require('object-hash');
-const { watch } = require('fs/promises');
+const path = require('path');
+const compression = require('compression');
+const express = require('express');
+const apiServer = require('./apiServer');
 
-const cmdAsync = util.promisify(nodecmd.get);
-const apiPort = userconfig.initial.apiport || config.server.apiport;
-const homePort = +apiPort - 1;
-const apiPortHttps = +apiPort + 1;
-let initialHash = hash(fs.readFileSync(path.join(__dirname, '/config/userconfig.js')));
-
-async function configReload() {
-  try {
-    const watcher = watch(path.join(__dirname, '/config'));
-    // eslint-disable-next-line
-    for await (const event of watcher) {
-      if (event.eventType === 'change' && event.filename === 'userconfig.js') {
-        const hashCurrent = hash(fs.readFileSync(path.join(__dirname, '/config/userconfig.js')));
-        if (hashCurrent === initialHash) {
-          return;
-        }
-        initialHash = hashCurrent;
-        log.info(`Config file changed, reloading ${event.filename}...`);
-        delete require.cache[require.resolve('./config/userconfig')];
-        // eslint-disable-next-line
-        userconfig = require('./config/userconfig');
-      }
+const args = parseArgs({
+  options: {
+    apiServer: {
+      type: "boolean",
+      short: "a",
+      default: false
     }
-  } catch (error) {
-    log.error(`Error watching files: ${error}`);
   }
-}
-
-setInterval(async () => {
-  configReload();
-}, 2 * 1000);
+}, process.argv.slice(2));
 
 async function initiate() {
-  if (!config.server.allowedPorts.includes(+apiPort)) {
-    log.error(`Flux port ${apiPort} is not supported. Shutting down.`);
-    process.exit();
-  }
-  if (userconfig.initial.apiport && userconfig.initial.apiport !== config.server.apiport) {
-    const verifyUpnp = await upnpService.verifyUPNPsupport(apiPort);
-    if (verifyUpnp !== true) {
-      log.error(`Flux port ${userconfig.initial.apiport} specified but UPnP failed to verify support. Shutting down.`);
-      process.exit();
-    }
-    const setupUpnp = await upnpService.setupUPNP(apiPort);
-    if (setupUpnp !== true) {
-      log.error(`Flux port ${userconfig.initial.apiport} specified but UPnP failed to map to api or home port. Shutting down.`);
-      process.exit();
-    }
-  }
+  const apiPort = await apiServer.initiate();
 
-  const server = app.listen(apiPort, () => {
-    log.info(`Flux running on port ${apiPort}!`);
-    serviceManager.startFluxFunctions();
-  });
+  if (!args.values.apiServer) {
+    const homePort = +apiPort - 1;
+    // Flux Home configuration
+    const home = path.join(__dirname, './HomeUI/dist');
 
-  socket.initIO(server);
+    const homeApp = express();
+    homeApp.use(compression());
+    homeApp.use(express.static(home));
 
-  try {
-    const certExists = fs.existsSync(path.join(__dirname, './certs/v1.key'));
-    if (!certExists) {
-      const nodedpath = path.join(__dirname, './helpers');
-      const exec = `cd ${nodedpath} && bash createSSLcert.sh`;
-      await cmdAsync(exec);
-    }
-    const key = fs.readFileSync(path.join(__dirname, './certs/v1.key'), 'utf8');
-    const cert = fs.readFileSync(path.join(__dirname, './certs/v1.crt'), 'utf8');
-    const credentials = { key, cert };
-    const httpsServer = https.createServer(credentials, app);
-    httpsServer.listen(apiPortHttps, () => {
-      log.info(`Flux https listening on port ${apiPortHttps}!`);
+    homeApp.get('/robots.txt', (req, res) => {
+      res.type('text/plain');
+      res.send('User-agent: *\nDisallow: /');
     });
-  } catch (error) {
-    log.error(error);
+
+    homeApp.get('*', (req, res) => {
+      res.sendFile(path.join(home, 'index.html'));
+    });
+
+    homeApp.listen(homePort, () => {
+      log.info(`Flux Home running on port ${homePort}!`);
+    });
   }
-
-  // Flux Home configuration
-  const home = path.join(__dirname, './HomeUI/dist');
-
-  const homeApp = express();
-  homeApp.use(compression());
-  homeApp.use(express.static(home));
-
-  homeApp.get('/robots.txt', (req, res) => {
-    res.type('text/plain');
-    res.send('User-agent: *\nDisallow: /');
-  });
-
-  homeApp.get('*', (req, res) => {
-    res.sendFile(path.join(home, 'index.html'));
-  });
-
-  homeApp.listen(homePort, () => {
-    log.info(`Flux Home running on port ${homePort}!`);
-  });
 }
 
 initiate();
