cd312f73a3fac760cce6b8ce9eb0d7965923d5b5
Cabecinha84 <valter.cabecinha@gmail.com>
2023-12-08 10:22:40 +0000

    cleanUp code no longer needed

--- ZelBack/src/services/fluxCommunication.js
+++ ZelBack/src/services/fluxCommunication.js
@@ -529,7 +529,7 @@ async function removeIncomingPeer(req, res, expressWS) {
  * To initiate and handle a connection. Opens a web socket and handles various events during connection.
  * @param {string} connection IP address (and port if applicable).
  */
-let socketPortsInformationActive = false;
+let myPort = null;
 async function initiateAndHandleConnection(connection) {
   let ip = connection;
   let port = config.server.apiport;
@@ -538,20 +538,12 @@ async function initiateAndHandleConnection(connection) {
       ip = connection.split(':')[0];
       port = connection.split(':')[1];
     }
-    let wsuri = `ws://${ip}:${port}/ws/flux/`;
-    if (!socketPortsInformationActive) {
-      const syncStatus = daemonServiceMiscRpcs.isDaemonSynced();
-      const daemonHeight = syncStatus.data.height || 0;
-      if (daemonHeight >= config.socketPortsInformation) {
-        socketPortsInformationActive = true;
-      }
-    }
-    if (socketPortsInformationActive) {
+    if (!myPort) {
       const myIP = await fluxNetworkHelper.getMyFluxIPandPort();
-      const myPort = myIP.split(':')[1] || 16127;
-      wsuri = `ws://${ip}:${port}/ws/flux/${myPort}`;
+      myPort = myIP.split(':')[1] || 16127;
+      log.info(`initiateAndHandleConnection myPort:${myPort}`);
     }
-
+    const wsuri = `ws://${ip}:${port}/ws/flux/${myPort}`;
     const websocket = new WebSocket(wsuri);
     websocket.port = port;
     websocket.ip = ip;
