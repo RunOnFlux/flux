a60b305b216aad362886b18a9c041a7967cf31f1
Cabecinha84 <valter.cabecinha@gmail.com>
2023-12-13 14:21:10 +0000

    fix

--- app.js
+++ app.js
@@ -1,47 +1,33 @@
 process.env.NODE_CONFIG_DIR = `${__dirname}/ZelBack/config/`;
 
-const { parseArgs } = require('node:util');
 const log = require('./ZelBack/src/lib/log');
 const path = require('path');
 const compression = require('compression');
 const express = require('express');
 const apiServer = require('./apiServer');
 
-const args = parseArgs({
-  options: {
-    apiServer: {
-      type: 'boolean',
-      short: 'a',
-      default: false,
-    },
-  },
-}, process.argv.slice(2));
-
 async function initiate() {
   const apiPort = await apiServer.initiate();
+  const homePort = +apiPort - 1;
+  // Flux Home configuration
+  const home = path.join(__dirname, './HomeUI/dist');
 
-  if (!args.values.apiServer) {
-    const homePort = +apiPort - 1;
-    // Flux Home configuration
-    const home = path.join(__dirname, './HomeUI/dist');
+  const homeApp = express();
+  homeApp.use(compression());
+  homeApp.use(express.static(home));
 
-    const homeApp = express();
-    homeApp.use(compression());
-    homeApp.use(express.static(home));
+  homeApp.get('/robots.txt', (req, res) => {
+    res.type('text/plain');
+    res.send('User-agent: *\nDisallow: /');
+  });
 
-    homeApp.get('/robots.txt', (req, res) => {
-      res.type('text/plain');
-      res.send('User-agent: *\nDisallow: /');
-    });
+  homeApp.get('*', (req, res) => {
+    res.sendFile(path.join(home, 'index.html'));
+  });
 
-    homeApp.get('*', (req, res) => {
-      res.sendFile(path.join(home, 'index.html'));
-    });
-
-    homeApp.listen(homePort, () => {
-      log.info(`Flux Home running on port ${homePort}!`);
-    });
-  }
+  homeApp.listen(homePort, () => {
+    log.info(`Flux Home running on port ${homePort}!`);
+  });
 }
 
 initiate();
