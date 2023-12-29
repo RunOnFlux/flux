738a634519e8626d70bdcc0d6957fe3e7b987234
Cabecinha84 <valter.cabecinha@gmail.com>
2023-12-29 09:25:45 +0000

    update echo

--- ZelBack/src/services/appsService.js
+++ ZelBack/src/services/appsService.js
@@ -1924,7 +1924,7 @@ async function createAppVolume(appSpecifications, appName, isComponent, res) {
           res.write(serviceHelper.ensureString(stFolderCreation2));
         }
         if (appId.toLowerCase().includes('minecraft')) {
-          const stignore = `sudo echo '*.paused' > ${appsFolder + appId + containerFolder}/.stignore`;
+          const stignore = `sudo echo '*.paused' >| ${appsFolder + appId + containerFolder}/.stignore`;
           log.info(stignore);
           // eslint-disable-next-line no-await-in-loop
           await cmdAsync(stignore);
