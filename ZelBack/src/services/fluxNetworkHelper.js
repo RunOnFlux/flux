fde57b3e20279161938f3becd38de4293d4b72a0
Cabecinha84 <valter.cabecinha@gmail.com>
2024-01-21 14:40:57 +0000

    give docker containers access to dns

--- ZelBack/src/services/fluxNetworkHelper.js
+++ ZelBack/src/services/fluxNetworkHelper.js
@@ -1341,6 +1341,8 @@ async function removeDockerContainerAccessToHost() {
     await cmdAsync(dropAccessToHostNetwork).catch((error) => log.error(`Error executing dropAccessToHostNetwork command:${error}`));
     const giveHostAccessToDockerNetwork = "sudo iptables -I FORWARD -i DOCKER-USER -d $(ip route | grep \"src $(ip addr show dev $(ip route | awk '/default/ {print $5}') | grep \"inet\" | awk 'NR==1{print $2}' | cut -d'/' -f 1)\" | awk '{print $1}') -m state --state ESTABLISHED,RELATED -j ACCEPT";
     await cmdAsync(giveHostAccessToDockerNetwork).catch((error) => log.error(`Error executing giveHostAccessToDockerNetwork command:${error}`));
+    const giveContainerAccessToDNS = "sudo iptables -I DOCKER-USER -p udp -d $(ip route | grep \"src $(ip addr show dev $(ip route | awk '/default/ {print $5}') | grep \"inet\" | awk 'NR==1{print $2}' | cut -d'/' -f 1)\" | awk '{print $1}') --dport 53 -j ACCEPT";
+    await cmdAsync(giveContainerAccessToDNS).catch((error) => log.error(`Error executing giveContainerAccessToDNS command:${error}`));
     log.info('Access to host from containers removed');
   } catch (error) {
     log.error(error);
