69c68ae0def4b90d6c9479a395751538e94c1e32
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-01-25 04:31:39 +0100

    button correction

--- HomeUI/src/views/apps/Management.vue
+++ HomeUI/src/views/apps/Management.vue
@@ -2017,7 +2017,7 @@
                           <b-button
                             variant="outline-danger"
                             class="d-flex justify-content-center align-items-center"
-                            style="width: 15px; height: 25px"
+                            style="width: 95px; height: 25px"
 
                             @click="deleteItem(row.index, newComponents)"
                           >
@@ -5369,7 +5369,9 @@ export default {
         { key: 'component_name', label: 'Component Name', thStyle: { width: '30%' } },
         { key: 'file_url', label: 'File URL', thStyle: { width: '55%' } },
         { key: 'file_size', label: 'File Size', thStyle: { width: '10%' } },
-        { key: 'actions', label: 'Actions', thStyle: { width: '5%' } },
+        {
+          key: 'actions', label: 'Actions', thStyle: { width: '5%' }, class: 'text-center',
+        },
       ];
     },
     // eslint-disable-next-line vue/no-dupe-keys
