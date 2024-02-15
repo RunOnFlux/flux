d3eb34716b05b20fb127bb2484627d4a758d412f
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-02-15 16:48:13 +0100

    Disable urlencoded

--- ZelBack/src/lib/server.js
+++ ZelBack/src/lib/server.js
@@ -9,7 +9,7 @@ const { app } = expressWs;
 
 app.use(morgan('combined'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 require('../routes')(app, expressWs);
