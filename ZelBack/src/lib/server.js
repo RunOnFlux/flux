5974ddcd927c22341e349883d9975eac05bfebf4
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-02-19 08:29:42 +0100

    removed body-parser

--- ZelBack/src/lib/server.js
+++ ZelBack/src/lib/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const eWS = require('express-ws');
-// const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 
