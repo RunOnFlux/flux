0fed8ed55198b8e5476e9ab3888e8633e9952c7f
David White <david.white@trustpower.co.nz>
2024-03-07 10:46:12 +0000

    Update devicehelper to use runCommand

--- ZelBack/src/services/deviceHelper.js
+++ ZelBack/src/services/deviceHelper.js
@@ -1,13 +1,16 @@
-const nodecmd = require('node-cmd');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const util = require('util');
+const serviceHelper = require('./serviceHelper');
 
-const cmdAsync = util.promisify(nodecmd.get);
+function escapeRegExp(string) {
+  // $& means the whole matched string
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
 
 async function getDfDevice(directory) {
-  const command = `df --output=source,target ${directory}`;
-  const response = await cmdAsync(command);
-  const lines = response.trim().split('\n');
+  const { stdout } = await serviceHelper.runCommand('df', {
+    params: ['--output=source,target', directory],
+  });
+
+  const lines = stdout.trim().split('\n');
   for (let i = 1; i < lines.length; i += 1) {
     const columns = lines[i].split(/\s+/);
     if (columns[0] !== '') {
@@ -18,13 +21,21 @@ async function getDfDevice(directory) {
 }
 
 async function hasQuotaOptionForDevice(device) {
-  const command = `mount | grep "${device}"`;
-  const response = await cmdAsync(command);
-  const mountInfo = response.trim();
-  if (mountInfo.includes('quota')) {
-    return true;
-  }
-  return false;
+  const { stdout } = await serviceHelper.runCommand('mount');
+
+  // assuming device is at the start of the line
+  const lookahead = '(?=.*quota)'
+  const re = new RegExp(lookahead + escapeRegExp(device))
+
+  return Boolean(stdout.match(re));
+
+  // const command = `mount | grep "${device}"`;
+  // const response = await cmdAsync(command);
+  // const mountInfo = response.trim();
+  // if (mountInfo.includes('quota')) {
+  //   return true;
+  // }
+  // return false;
 }
 module.exports = {
   getDfDevice,
