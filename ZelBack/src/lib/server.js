704dfa4b87bc2b1cf62aaa9b2aa061e3eefc7d8c
TheTrunk <tadeas.kmenta@zel.cash>
2024-03-16 14:57:49 +0700

    add missing compression to api

--- ZelBack/src/lib/server.js
+++ ZelBack/src/lib/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const eWS = require('express-ws');
 const cors = require('cors');
 const morgan = require('morgan');
+const compression = require('compression');
 
 const expressWs = eWS(express());
 const { app } = expressWs;
 
+app.use(compression());
 app.use(morgan('combined'));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
