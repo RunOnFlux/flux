b6007407773269e53e1ff04263eebb6eb3c0be37
Cabecinha84 <valter.cabecinha@gmail.com>
2024-05-02 15:32:44 +0100

    drop zelnodetransactions mongodb collection

--- ZelBack/src/routes.js
+++ ZelBack/src/routes.js
@@ -387,9 +387,10 @@ module.exports = (app, expressWs) => {
   //   explorerService.getAllFluxTransactions(req, res);
   // });
   // filter can be IP, address, collateralHash.
+  /*
   app.get('/explorer/fluxtxs/:filter?', cache('30 seconds'), (req, res) => {
     explorerService.getFilteredFluxTxs(req, res);
-  });
+  }); */
   app.get('/explorer/utxo/:address?', cache('30 seconds'), (req, res) => {
     explorerService.getAddressUtxos(req, res);
   });
