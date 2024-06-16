c76ee738e3b483e126aed798e11c10221c58a99c
David White <david@runonflux.io>
2024-06-16 17:13:50 +0100

    Add tests

---
+++ tests/unit/socketIoServer.test.js
@@ -0,0 +1,61 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const { Server } = require('node:http');
+
+const proxyquire = require('proxyquire').noCallThru();
+
+const handlerA = sinon.stub();
+const handlerB = sinon.stub();
+const handlers = { a: handlerA, b: handlerB };
+
+const io = proxyquire('../../ZelBack/src/lib/socketIoServer', { './socketIoHandlers/index': handlers });
+
+describe('requestHistory tests', () => {
+  beforeEach(async () => { });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should set correct default properties when no parameters used', () => {
+    const httpServer = new Server();
+    const ioServer = new io.SocketIoServer(httpServer);
+    const errorHandlers = ioServer.io.listeners('error');
+
+    expect(ioServer.io.opts.transports).to.deep.equal(['websocket', 'polling', 'flashsocket']);
+    expect(ioServer.io.opts.cors).to.deep.equal({ origin: '*', methods: ['GET', 'POST'] });
+    expect(errorHandlers.length).to.equal(1);
+  });
+
+  it('should set correct properties when parameters used', () => {
+    let logged = false;
+    const errorHandler = () => { logged = true; };
+
+    const httpServer = new Server();
+    const ioServer = new io.SocketIoServer(httpServer, { transports: ['websocket'], cors: { origin: '/test', methods: ['GET'] }, errorHandler });
+    const errorHandlers = ioServer.io.listeners('error');
+
+    expect(ioServer.io.opts.transports).to.deep.equal(['websocket']);
+    expect(ioServer.io.opts.cors).to.deep.equal({ origin: '/test', methods: ['GET'] });
+
+    // this would be a lot easier to test if the error event called the handler direclty
+    errorHandlers[0]();
+    expect(logged).to.equal(true);
+  });
+
+  it('should add all handlers from socketIoHandlers', () => {
+    const httpServer = new Server();
+    const ioServer = new io.SocketIoServer(httpServer);
+
+    ioServer.listen();
+
+    const aListeners = ioServer.getListenersByNamespace('a');
+    const bListeners = ioServer.getListenersByNamespace('b');
+
+    expect(aListeners.length).to.equal(1);
+    expect(bListeners.length).to.equal(1);
+    expect(aListeners[0]).to.be.equal(handlerA);
+    expect(bListeners[0]).to.be.equal(handlerB);
+  });
+});
