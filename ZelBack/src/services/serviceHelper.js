1c2bd5d3d1093d10d90ff267247c3952a37b15dd
David White <david@runonflux.io>
2024-06-17 15:20:56 +0100

    Patch imageVerifier to use serviceHelper axios

--- ZelBack/src/services/serviceHelper.js
+++ ZelBack/src/services/serviceHelper.js
@@ -202,7 +202,7 @@ async function axiosPost(url, data, userOptions = {}) {
  * @param {object} options Standard axios options
  * @returns {object} AxiosInstance
  */
-function createAxiosinstance(userOptions = {}) {
+function axiosInstance(userOptions = {}) {
   const options = { ...userOptions };
 
   if (!options.signal) options.signal = shc.signal;
@@ -452,7 +452,7 @@ module.exports = {
   axiosGet,
   axiosPost,
   commandStringToArray,
-  createAxiosinstance,
+  axiosInstance,
   delay,
   deleteLoginPhrase,
   dockerBufferToString,
