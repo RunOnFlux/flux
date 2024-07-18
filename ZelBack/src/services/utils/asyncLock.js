a0be69cacaf842b8998f77490619af0799fb4602
David White <david@runonflux.io>
2024-07-18 11:35:29 +0100

    udpate async lock

--- ZelBack/src/services/utils/asyncLock.js
+++ ZelBack/src/services/utils/asyncLock.js
@@ -28,6 +28,36 @@ class AsyncLock {
     this.ready = Promise.resolve();
     this.locked = false;
   }
+
+  /**
+   * Waits for the lock to be ready, with a max wait time. This
+   * will unlock lock after timeout
+   * @param {number} ms Max milliseconds to wait
+   * @returns {Promise<void>}
+   */
+  async unlockTimeout(ms) {
+    if (!this.locked) return;
+    const timeout = setTimeout(() => {
+      this.disable();
+    }, ms);
+    await this.ready;
+    clearTimeout(timeout);
+  }
+
+  /**
+   * Similar to unlockTimeout, waits for the lock to be ready,
+   * with a max wait time. Does not unlock the lock after the timeout
+   * @param {number} ms Max milliseconds to wait
+   * @returns {Promise<void>}
+   */
+  async readyTimeout(ms) {
+    if (!this.locked) return;
+
+    let timeout;
+    const sleep = new Promise((r) => { timeout = setTimeout(r, ms); });
+    await Promise.race([this.ready, sleep]);
+    clearTimeout(timeout);
+  }
 }
 
 module.exports = { AsyncLock };
