790ac0b60c385c7e6c770d1c548b8da5d2ded059
Jeff Vader <simon@sygem.com>
2024-07-23 12:14:47 +0100

    Don't compress response if the x-no-compression header is present (#1376)

--- ZelBack/src/lib/fluxServer.js
+++ ZelBack/src/lib/fluxServer.js
@@ -23,7 +23,16 @@ class FluxServer {
   static supportedModes = ['http', 'https'];
 
   static defaultMiddlewares = [
-    compression(),
+    compression(compression({
+      filter: (req, res) => {
+        if (req.headers['x-no-compression']) {
+          // don't compress responses with this request header
+          return false;
+        }
+        // fallback to standard filter function
+        return compression.filter(req, res);
+      },
+    })),
     morgan('combined'),
     express.json(),
     cors(),
