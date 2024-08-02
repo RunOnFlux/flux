dd9a2ed8417e014954a43150d4394d422c4809db
Cabecinha84 <valter.cabecinha@gmail.com>
2024-08-02 10:09:02 +0100

    working, cleaning up the logs

--- ZelBack/src/services/appsService.js
+++ ZelBack/src/services/appsService.js
@@ -4764,23 +4764,16 @@ async function checkAppSecrets(appName, appComponentSpecs, registration = false)
   const query = {};
   const projection = { projection: { _id: 0 } };
   const results = await dbHelper.findInDatabase(database, globalAppsInformation, query, projection);
-  log.info(`checkAppSecrets: ${appName} lets search on db for the secrets: ${JSON.stringify(appComponentSpecs.secrets.replace(/(\r\n|\n|\r)/gm, '').replace(/\\/g, ''))}`);
   // eslint-disable-next-line no-restricted-syntax
   for (const app of results) {
     if (app.version >= 7 && app.nodes.length > 0) {
       // eslint-disable-next-line no-restricted-syntax
       for (const component of app.compose) {
-        if (component.secrets.length > 0) {
-          if (app.name === 'BrokerNode1677472532536') {
-            log.info(`checkAppSecrets: component ${component.name} of appName same secrets on app: ${app.name} secrets: ${JSON.stringify(component.secrets.replace(/(\r\n|\n|\r)/gm, '').replace(/\\/g, ''))}`);
-          }
-          if (JSON.stringify(component.secrets.replace(/(\r\n|\n|\r)/gm, '').replace(/\\/g, '')) === JSON.stringify(appComponentSpecs.secrets.replace(/(\r\n|\n|\r)/gm, '').replace(/\\/g, ''))) {
-            log.info(`checkAppSecrets: ${appName} Found same secrets on app: ${app.name} registration: ${registration}`);
-            if (registration) {
-              throw new Error(`Provided component ${component.name} secrets are not valid`);
-            } else if (app.name !== appName) {
-              throw new Error(`Provided component ${component.name} secrets are not valid`);
-            }
+        if (component.secrets.length > 0 && JSON.stringify(component.secrets.replace(/(\r\n|\n|\r)/gm, '').replace(/\\/g, '')) === JSON.stringify(appComponentSpecs.secrets.replace(/(\r\n|\n|\r)/gm, '').replace(/\\/g, ''))) {
+          if (registration) {
+            throw new Error(`Provided component ${component.name} secrets are not valid`);
+          } else if (app.name !== appName) {
+            throw new Error(`Provided component ${component.name} secrets are not valid`);
           }
         }
       }
@@ -10421,11 +10414,9 @@ async function verifyAppUpdateParameters(req, res) {
       await verifyAppSpecifications(appSpecFormatted, daemonHeight, true);
 
       if (appSpecFormatted.version >= 7 && appSpecFormatted.nodes.length > 0) {
-        log.info(`verifyAppUpdateParameters: ${appSpecFormatted.name} is enterprise app.`);
         // eslint-disable-next-line no-restricted-syntax
         for (const appComponent of appSpecFormatted.compose) {
           if (appComponent.secrets.length > 0) {
-            log.info(`verifyAppUpdateParameters: ${appSpecFormatted.name} have secrets.`);
             // eslint-disable-next-line no-await-in-loop
             await checkAppSecrets(appSpecFormatted.name, appComponent, false);
           }
