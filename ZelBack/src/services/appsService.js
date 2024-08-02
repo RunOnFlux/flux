192913129cd12aef43284cf81daafbbd1d94c01f
Cabecinha84 <valter.cabecinha@gmail.com>
2024-08-02 08:52:39 +0100

    add logs

--- ZelBack/src/services/appsService.js
+++ ZelBack/src/services/appsService.js
@@ -4770,6 +4770,7 @@ async function checkAppSecrets(appName, appComponentSpecs, registration = false)
       // eslint-disable-next-line no-restricted-syntax
       for (const component of app.compose) {
         if (component.secrets.length > 0 && component.secrets === appComponentSpecs.secrets) {
+          log.info(`checkAppSecrets: ${appName} Found same secrets on app: ${app.name} registration: ${registration}`);
           if (registration) {
             throw new Error(`Provided component ${component.name} secrets are not valid`);
           } else if (app.name !== appName) {
