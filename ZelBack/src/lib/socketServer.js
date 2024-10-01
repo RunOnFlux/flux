805afd1b7e72d93411722715d72f0d08bd8c2bce
Cabecinha84 <valter.cabecinha@gmail.com>
2024-10-01 10:45:45 +0100

    test

--- ZelBack/src/lib/socketServer.js
+++ ZelBack/src/lib/socketServer.js
@@ -4,7 +4,30 @@ const WebSocketServer = require('ws').Server;
 class FluxWebsocketServer {
   static defautlErrorHandler = () => { };
 
-  #socketServer = new WebSocketServer({ noServer: true });
+  compression = { };
+
+  #socketServer = new WebSocketServer({
+    noServer: true,
+    perMessageDeflate: {
+      zlibDeflateOptions: {
+      // See zlib defaults.
+        chunkSize: 1024,
+        memLevel: 7,
+        level: 3,
+      },
+      zlibInflateOptions: {
+        chunkSize: 10 * 1024,
+      },
+      // Other options settable:
+      clientNoContextTakeover: true, // Defaults to negotiated value.
+      serverNoContextTakeover: true, // Defaults to negotiated value.
+      serverMaxWindowBits: 10, // Defaults to negotiated value.
+      // Below options specified as default values.
+      concurrencyLimit: 10, // Limits zlib concurrency for perf.
+      threshold: 1024, // Size (in bytes) below which messages
+    // should not be compressed if context takeover is disabled.
+    },
+  });
 
   #routes = {};
 
--- ZelBack/src/services/fluxCommunication.js
+++ ZelBack/src/services/fluxCommunication.js
@@ -679,7 +679,25 @@ async function initiateAndHandleConnection(connection) {
       myPort = myIP.split(':')[1] || 16127;
     }
     const options = {
-      perMessageDeflate: true,
+      perMessageDeflate: {
+        zlibDeflateOptions: {
+        // See zlib defaults.
+          chunkSize: 1024,
+          memLevel: 7,
+          level: 3,
+        },
+        zlibInflateOptions: {
+          chunkSize: 10 * 1024,
+        },
+        // Other options settable:
+        clientNoContextTakeover: true, // Defaults to negotiated value.
+        serverNoContextTakeover: true, // Defaults to negotiated value.
+        serverMaxWindowBits: 10, // Defaults to negotiated value.
+        // Below options specified as default values.
+        concurrencyLimit: 10, // Limits zlib concurrency for perf.
+        threshold: 1024, // Size (in bytes) below which messages
+      // should not be compressed if context takeover is disabled.
+      },
     };
     const wsuri = `ws://${ip}:${port}/ws/flux/${myPort}`;
     const websocket = new WebSocket(wsuri, options);
