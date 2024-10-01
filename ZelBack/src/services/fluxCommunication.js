360522e25180e32de266829ee461763bb2b37b0e
Cabecinha84 <valter.cabecinha@gmail.com>
2024-10-01 09:32:01 +0100

    test change

--- ZelBack/src/services/fluxCommunication.js
+++ ZelBack/src/services/fluxCommunication.js
@@ -680,11 +680,23 @@ async function initiateAndHandleConnection(connection) {
     }
     const options = {
       perMessageDeflate: {
-        enabled: true,
-        clientNoCompression: false,
-        clientMaxWindowBits: 18,
-        serverNoCompression: false,
-        serverMaxWindowBits: 18,
+        zlibDeflateOptions: {
+          // See zlib defaults.
+          chunkSize: 1024,
+          memLevel: 7,
+          level: 3,
+        },
+        zlibInflateOptions: {
+          chunkSize: 10 * 1024,
+        },
+        // Other options settable:
+        clientNoContextTakeover: true, // Defaults to negotiated value.
+        serverNoContextTakeover: true, // Defaults to negotiated value.
+        serverMaxWindowBits: 16, // Defaults to negotiated value.
+        // Below options specified as default values.
+        concurrencyLimit: 10, // Limits zlib concurrency for perf.
+        threshold: 1024, // Size (in bytes) below which messages
+        // should not be compressed.
       },
     };
     const wsuri = `ws://${ip}:${port}/ws/flux/${myPort}`;
