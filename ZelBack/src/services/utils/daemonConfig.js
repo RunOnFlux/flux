d2aded02d465fd966aa9b105b84834a1a9c1e1f9
David White <david@runonflux.io>
2024-10-08 12:57:20 +0100

    Add zelpath back in

--- ZelBack/src/services/utils/daemonConfig.js
+++ ZelBack/src/services/utils/daemonConfig.js
@@ -5,6 +5,7 @@ const os = require('node:os');
 class DaemonConfig {
   static daemonConfigPaths = {
     linux: {
+      zelPath: '/dat/var/lib/fluxd/flux.conf',
       fluxPath: '/dat/var/lib/fluxd/flux.conf',
     },
     darwin: {
