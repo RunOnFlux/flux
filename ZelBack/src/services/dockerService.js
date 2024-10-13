d9bb676d59260c2a0e68a9b0389e8922534ef4fa
Cabecinha84 <valter.cabecinha@gmail.com>
2024-10-13 20:42:36 +0100

    revert

--- ZelBack/src/services/dockerService.js
+++ ZelBack/src/services/dockerService.js
@@ -657,8 +657,7 @@ async function appDockerCreate(appSpecifications, appName, isComponent, fullAppS
     Tty: false,
     ExposedPorts: exposedPorts,
     HostConfig: {
-      CpuQuota: Math.round(appSpecifications.cpu * 100000),
-      CpuPeriod: 100000,
+      NanoCPUs: Math.round(appSpecifications.cpu * 1e9),
       Memory: Math.round(appSpecifications.ram * 1024 * 1024),
       MemorySwap: Math.round((appSpecifications.ram + (config.fluxapps.defaultSwap * 1000)) * 1024 * 1024), // default 2GB swap
       // StorageOpt: { size: '5G' }, // root fs has max default 5G size, v8 is 5G + specified as per config.fluxapps.hddFileSystemMinimum
