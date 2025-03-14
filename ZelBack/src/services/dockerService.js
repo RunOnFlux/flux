e5886e826bafb1f7e92c976201779770063ad5c6
Cabecinha84 <valter.cabecinha@gmail.com>
2025-03-14 22:48:07 +0000

    revert change

--- ZelBack/src/services/dockerService.js
+++ ZelBack/src/services/dockerService.js
@@ -845,9 +845,8 @@ async function appDockerCreate(appSpecifications, appName, isComponent, fullAppS
       Config: {
         'syslog-address': `udp://${syslogIP}:514`,
         'syslog-facility': 'local0',
-        tag: `${appName}`,
+        tag: `${appName}#${appSpecifications.name}#${nodeId}#${nodeIP}`,
         'syslog-format': 'rfc5424',
-        labels: `$component_name:${appSpecifications.name},host_id:${nodeId},host_ip:${nodeIP}`,
       },
     }
     : {
