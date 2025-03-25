75adbf291f4a9a42e4b53f92370d3552aa4b9d57
Cabecinha84 <valter.cabecinha@gmail.com>
2025-03-25 08:18:39 +0000

    adjust some delays on trySpawningGlobalApplication

--- ZelBack/src/services/appsService.js
+++ ZelBack/src/services/appsService.js
@@ -9283,13 +9283,13 @@ async function trySpawningGlobalApplication() {
       log.info(`trySpawningGlobalApplication - Application ${appToRun} selected to try to spawn. Reported as been running in ${appToRunAux.actual} instances and ${appToRunAux.required} are required.`);
       if (appToRunAux.required === appToRunAux.actual + 1 && appToRunAux.nodes.length === 0 && Math.random() > 0.4) {
         log.info('trySpawningGlobalApplication - app missing one instance failed the 40% probability check to install');
-        await serviceHelper.delay(30 * 60 * 1000);
+        await serviceHelper.delay(10 * 60 * 1000);
         trySpawningGlobalApplication();
         return;
       }
       if (appToRunAux.required === appToRunAux.actual + 2 && appToRunAux.nodes.length === 0 && Math.random() > 0.75) {
         log.info('trySpawningGlobalApplication - app missing two instances failed the 75% probability check to install');
-        await serviceHelper.delay(30 * 60 * 1000);
+        await serviceHelper.delay(10 * 60 * 1000);
         trySpawningGlobalApplication();
         return;
       }
