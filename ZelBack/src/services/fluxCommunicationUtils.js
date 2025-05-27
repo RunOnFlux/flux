5a99c38a4063c1541fd27ecfa5212b8112e91ba8
Cabecinha84 <valter.cabecinha@gmail.com>
2025-05-27 21:31:36 +0100

    remove and add more logs

--- ZelBack/src/services/fluxCommunicationUtils.js
+++ ZelBack/src/services/fluxCommunicationUtils.js
@@ -113,10 +113,6 @@ async function verifyFluxBroadcast(data, obtainedFluxNodesList, currentTimeStamp
   const { signature } = dataObj;
   const { version } = dataObj;
 
-  const appregisterOrUpdate = dataObj.data && (dataObj.data.type === 'fluxappregister' || dataObj.data.type === 'fluxappupdate');
-  if (appregisterOrUpdate) {
-    log.info(`verifyFluxBroadcast - ${JSON.stringify(dataObj.data)}`);
-  }
   // only version 1 is active
   if (version !== 1) {
     return false;
@@ -127,9 +123,6 @@ async function verifyFluxBroadcast(data, obtainedFluxNodesList, currentTimeStamp
   currentTimeStamp = currentTimeStamp || Date.now(); // ms
   if (currentTimeStamp < (timestamp - 120000)) { // message was broadcasted in the future. Allow 120 sec clock sync
     log.error('Message from future');
-    if (appregisterOrUpdate) {
-      log.info('verifyFluxBroadcast - Message from future');
-    }
     return false;
   }
 
@@ -137,9 +130,6 @@ async function verifyFluxBroadcast(data, obtainedFluxNodesList, currentTimeStamp
   if (obtainedFluxNodesList) { // for test purposes.
     node = obtainedFluxNodesList.find((key) => key.pubkey === pubKey);
     if (!node) {
-      if (appregisterOrUpdate) {
-        log.info('verifyFluxBroadcast - no Node test');
-      }
       return false;
     }
   }
@@ -183,18 +173,12 @@ async function verifyFluxBroadcast(data, obtainedFluxNodesList, currentTimeStamp
   }
   if (!node) {
     log.warn(`No node belonging to ${pubKey} found`);
-    if (appregisterOrUpdate) {
-      log.info('verifyFluxBroadcast - warning no node');
-    }
     return false;
   }
   const messageToVerify = version + message + timestamp;
   const verified = verificationHelper.verifyMessage(messageToVerify, pubKey, signature);
   if (verified === true) {
     return true;
-  }
-  if (appregisterOrUpdate) {
-    log.info('verifyFluxBroadcast - Message not verified');
   }
   return false;
 }
