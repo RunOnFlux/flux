65c7a97df941dc40ba9f70d8ec01cd16b4bfcc56
Cabecinha84 <valter.cabecinha@gmail.com>
2025-05-27 18:21:54 +0100

    add logs

--- ZelBack/src/services/fluxCommunication.js
+++ ZelBack/src/services/fluxCommunication.js
@@ -70,6 +70,7 @@ async function handleAppMessages(message, fromIP, port) {
     // do furtherVerification of message
     // eslint-disable-next-line global-require
     const appsService = require('./appsService');
+    log.info(JSON.stringify(message.data));
     const rebroadcastToPeers = await appsService.storeAppTemporaryMessage(message.data, true);
     if (rebroadcastToPeers === true) {
       const syncStatus = daemonServiceMiscRpcs.isDaemonSynced();
