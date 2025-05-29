b88d4d55bfc6ae84a29d25b8397003c0d6ce855e
Cabecinha84 <valter.cabecinha@gmail.com>
2025-05-29 10:09:34 +0100

    clean

--- ZelBack/src/services/fluxCommunicationUtils.js
+++ ZelBack/src/services/fluxCommunicationUtils.js
@@ -112,7 +112,6 @@ async function verifyFluxBroadcast(data, obtainedFluxNodesList, currentTimeStamp
   const { timestamp } = dataObj; // ms
   const { signature } = dataObj;
   const { version } = dataObj;
-
   // only version 1 is active
   if (version !== 1) {
     return false;
