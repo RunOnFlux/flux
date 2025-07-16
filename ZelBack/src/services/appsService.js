cbfc1ff53e3840ad4fdba81c0cea4306bca8ec9d
David White <david@runonflux.io>
2025-07-16 15:34:38 +0100

    Multiple fixes for the frontend spec reencryption / decryption

--- ZelBack/src/services/appsService.js
+++ ZelBack/src/services/appsService.js
@@ -7971,7 +7971,7 @@ async function decryptAesKeyWithRsaKey(appName, daemonHeight, enterpriseKey, own
     };
     const permanentAppMessage = await dbHelper.findInDatabase(database, globalAppsMessages, appsQuery, projection);
     const lastAppRegistration = permanentAppMessage[permanentAppMessage.length - 1];
-    appOwner = lastAppRegistration.owner;
+    appOwner = lastAppRegistration.appSpecifications.owner;
   }
   const inputData = JSON.stringify({
     fluxID: appOwner,
@@ -7983,10 +7983,9 @@ async function decryptAesKeyWithRsaKey(appName, daemonHeight, enterpriseKey, own
   const { status, data } = dataReturned;
   if (status === 'success') {
     const dataParsed = JSON.parse(data);
-    const base64AesKey = status === 'success' && dataParsed.status === 'ok' ? dataParsed.message : null;
-    if (base64AesKey) {
-      return base64AesKey;
-    }
+    const base64AesKey = dataParsed.status === 'ok' ? dataParsed.message : null;
+    if (base64AesKey) return base64AesKey;
+
     throw new Error('Error decrypting AES key.');
   } else {
     throw new Error('Error getting decrypted AES key.');
@@ -8202,8 +8201,15 @@ async function encryptEnterpriseFromSession(appSpec, daemonHeight, enterpriseKey
 
   const appName = appSpec.name;
 
+  const enterpriseSpec = {
+    contacts: appSpec.contacts,
+    compose: appSpec.compose,
+  };
+
+  const encoded = JSON.stringify(enterpriseSpec);
+
   const base64AesKey = await decryptAesKeyWithRsaKey(appName, daemonHeight, enterpriseKey);
-  const encryptedEnterprise = encryptWithAesSession(appSpec.enterprise, base64AesKey);
+  const encryptedEnterprise = encryptWithAesSession(appSpec.enterprise, encoded, base64AesKey);
   if (encryptedEnterprise) {
     return encryptedEnterprise;
   }
@@ -10479,7 +10485,7 @@ async function getApplicationSpecificationAPI(req, res) {
       throw new Error('Daemon not yet synced.');
     }
 
-    const { data: { daemonHeight } } = syncStatus;
+    const { data: { height: daemonHeight } } = syncStatus;
 
     let { appname, decrypt } = req.params;
     appname = appname || req.query.appname;
@@ -10557,6 +10563,7 @@ async function getApplicationSpecificationAPI(req, res) {
       daemonHeight,
       encryptedEnterpriseKey,
     );
+
     specifications.contacts = [];
     specifications.compose = [];
 
