ecb20852b278fbd917b75bd86fd1c69da8009577
David White <david@runonflux.io>
2025-09-13 12:31:57 +0100

    Overrides and config to separate vue / js

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,9 @@
 const path = require('path');
 const globals = require('globals');
+const importPlugin = require('eslint-plugin-import');
+const babelParser = require('@babel/eslint-parser');
 const vue = require('eslint-plugin-vue');
-const js = require('@eslint/js');
-const { FlatCompat } = require('@eslint/eslintrc');
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+const vueParser = require('vue-eslint-parser');
 
 module.exports = [
   {
@@ -22,12 +17,9 @@ module.exports = [
       'dev/',
     ],
   },
-  ...compat.extends(
-    'plugin:vue/recommended', 
-    '@vue/eslint-config-airbnb',
-    'prettier'
-  ),
+  // JavaScript files configuration ONLY (no Vue files)
   {
+    files: ['**/*.js'],
     languageOptions: {
       globals: {
         ...globals.es2020,
@@ -36,14 +28,13 @@ module.exports = [
         ...globals.mocha,
         userconfig: true,
       },
-      parser: require('vue-eslint-parser'),
+      parser: babelParser,
       parserOptions: {
-        parser: '@babel/eslint-parser',
         requireConfigFile: false,
       },
     },
     plugins: {
-      vue,
+      import: importPlugin,
     },
     rules: {
       'max-len': [
@@ -54,16 +45,106 @@ module.exports = [
           ignoreTrailingComments: true,
         },
       ],
-      'vue/max-len': [
+      'no-console': 'off',
+      'linebreak-style': ['error', 'unix'],
+      'prefer-destructuring': ['error', { object: true, array: false }],
+      'import/no-extraneous-dependencies': [
         'error',
         {
-          code: 120,
+          devDependencies: true,
+          optionalDependencies: true,
+          peerDependencies: false,
+        },
+      ],
+      camelcase: [
+        'error',
+        { properties: 'never', ignoreDestructuring: true, ignoreImports: true },
+      ],
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        { js: 'never' },
+      ],
+      'import/order': 'off',
+      complexity: [
+        'warn',
+        {
+          max: 25,
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.vue'],
+        },
+        alias: {
+          map: [
+            ['@', path.resolve(__dirname, './HomeUI/src')],
+            ['@themeConfig', path.resolve(__dirname, './HomeUI/themeConfig.js')],
+            ['@core', path.resolve(__dirname, './HomeUI/src/@core')],
+            [
+              '@validations',
+              path.resolve(__dirname, './HomeUI/src/@core/utils/validations/validations.js'),
+            ],
+            ['@axios', path.resolve(__dirname, './HomeUI/src/libs/axios')],
+            ['ZelBack', path.resolve(__dirname, './ZelBack')],
+            ['Config', path.resolve(__dirname, './config')],
+          ],
+          extensions: ['.js', '.jsx', '.vue'],
+        },
+      },
+    },
+  },
+  // Vue files ONLY - completely separate configuration
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      globals: {
+        ...globals.es2020,
+        ...globals.commonjs,
+        ...globals.node,
+        ...globals.mocha,
+        userconfig: true,
+      },
+      parser: vueParser,
+      parserOptions: {
+        parser: babelParser,
+        requireConfigFile: false,
+      },
+    },
+    plugins: {
+      vue,
+      import: importPlugin,
+    },
+    rules: {
+      // Vue recommended rules
+      ...vue.configs.recommended.rules,
+
+      // 140 character limit for Vue files
+      'max-len': [
+        'error',
+        {
+          code: 140,
           ignoreUrls: true,
           ignoreTrailingComments: true,
         },
       ],
-      'no-console': 'off',
+      'vue/max-len': [
+        'error',
+        {
+          code: 140,
+          ignoreUrls: true,
+          ignoreTrailingComments: true,
+        },
+      ],
+
+      // Vue-specific overrides
       'vue/no-use-v-if-with-v-for': 'off',
+      'vue/multi-word-component-names': 'off',
+
+      // General rules for Vue files
+      'no-console': 'off',
       'linebreak-style': ['error', 'unix'],
       'prefer-destructuring': ['error', { object: true, array: false }],
       'import/no-extraneous-dependencies': [
@@ -84,13 +165,14 @@ module.exports = [
         { vue: 'always', js: 'never' },
       ],
       'import/order': 'off',
-      'vue/multi-word-component-names': 'off',
       complexity: [
         'warn',
         {
-          max: 25, // Gradual improvement threshold - currently 103 violations would become ~30
+          max: 25,
         },
       ],
+
+      // Accessibility overrides for Vue
       'vuejs-accessibility/label-has-for': 'off',
       'vuejs-accessibility/click-events-have-key-events': 'off',
       'vuejs-accessibility/heading-has-content': 'off',
@@ -118,20 +200,6 @@ module.exports = [
           extensions: ['.js', '.jsx', '.vue'],
         },
       },
-    },
-  },
-  {
-    files: ['**/__tests__/*.{j,t}s?(x)'],
-    languageOptions: {
-      globals: {
-        ...globals.mocha,
-      },
-    },
-  },
-  {
-    files: ['*.html'],
-    rules: {
-      'vue/comment-directive': 'off',
     },
   },
 ];
