74348a91a8a5f42b2f3ab9a34e86772b765c5d17
Valter Silva <valter.cabecinha@gmail.com>
2025-10-29 17:22:54 +0000

    speed up forceAppRemovals calls for testing

--- ZelBack/src/services/serviceManager.js
+++ ZelBack/src/services/serviceManager.js
@@ -422,7 +422,7 @@ async function startFluxFunctions() {
       advancedWorkflows.forceAppRemovals(); // force cleanup of apps every day
       setInterval(() => {
         advancedWorkflows.forceAppRemovals();
-      }, 24 * 60 * 60 * 1000);
+      }, 30 * 60 * 60 * 1000);
     }, 30 * 60 * 1000);
     setTimeout(() => {
       appInspector.checkStorageSpaceForApps(
