cb6c8b5049e23dc6e9d295c00c07a8e18a697aae
Valter Silva <valter.cabecinha@gmail.com>
2026-01-21 10:29:27 +0000

    add vetted apps repos

--- ZelBack/src/services/appLifecycle/appSpawner.js
+++ ZelBack/src/services/appLifecycle/appSpawner.js
@@ -314,19 +314,25 @@ async function trySpawningGlobalApplication() {
     }
 
     // EARLY CHECK: Verify app doesn't use user-blocked ports before expensive Docker Hub operations
-    const appPorts = appUtilities.getAppPorts(appSpecifications);
-    // eslint-disable-next-line no-restricted-syntax
-    for (let i = 0; i < appPorts.length; i += 1) {
-      const port = appPorts[i];
-      const isUserBlocked = fluxNetworkHelper.isPortUserBlocked(port);
-      if (isUserBlocked) {
-        log.info(`trySpawningGlobalApplication - App ${appSpecifications.name} uses user-blocked port ${port}. Adding to error cache.`);
-        globalState.spawnErrorsLongerAppCache.set(appHash, '');
-        // eslint-disable-next-line no-await-in-loop
-        await serviceHelper.delay(shortDelayTime);
-        trySpawningGlobalApplication();
-        return;
+    // Skip this check for vetted apps
+    const appIsVetted = await imageManager.isAppVetted(appSpecifications);
+    if (!appIsVetted) {
+      const appPorts = appUtilities.getAppPorts(appSpecifications);
+      // eslint-disable-next-line no-restricted-syntax
+      for (let i = 0; i < appPorts.length; i += 1) {
+        const port = appPorts[i];
+        const isUserBlocked = fluxNetworkHelper.isPortUserBlocked(port);
+        if (isUserBlocked) {
+          log.info(`trySpawningGlobalApplication - App ${appSpecifications.name} uses user-blocked port ${port}. Adding to error cache.`);
+          globalState.spawnErrorsLongerAppCache.set(appHash, '');
+          // eslint-disable-next-line no-await-in-loop
+          await serviceHelper.delay(shortDelayTime);
+          trySpawningGlobalApplication();
+          return;
+        }
       }
+    } else {
+      log.info(`trySpawningGlobalApplication - App ${appSpecifications.name} is vetted. Bypassing user-blocked ports check.`);
     }
 
     // verify app compliance
